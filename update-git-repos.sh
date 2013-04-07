32308d4fd5f7cff399683f036fa1e5c99488aeb3
Peter Hillerström <peter.hillerstrom@gmail.com>
2013-04-08 00:01:47 +0300

    Fix directory path handling when given absolute path. Print usage when no git repositories are found.

--- update-git-repos.sh
+++ update-git-repos.sh
@@ -4,11 +4,15 @@
 set -eu
 
 curdir=`pwd`
-trap 'cd $curdir; printf "\nExit by signal or error"; exit 1' EXIT
-
 target=${1:-}
-target=`echo $curdir/$target | perl -pe 's/\/$//g'`  # Strip trailing slash
 
+trap 'cd $curdir; printf "\nAll done.\n"' EXIT
+
+
+if [[ `echo $target | cut -c1` != '/' ]]
+then
+    target=`echo $curdir/$target | perl -pe 's/\/$//g'`  # Strip trailing slash
+fi
 
 current_branch () {
 	ref=$(git symbolic-ref HEAD 2> /dev/null)  || ref=$(git rev-parse --short HEAD 2> /dev/null)  || return
@@ -16,19 +20,23 @@ current_branch () {
 }
 
 
-echo "--> Using target directory tree: $target"
+dirs=`find $target -name '.git' -print | perl -pe 's/.git$//g'`
 
-for dir in `find $target -name '.git' -print | perl -pe 's/.git$//g'`
-do
-    echo "--> Updating: $dir"
-    cd $dir
-    git pull origin $(current_branch)
-    echo
-    cd $curdir
-done
-
-
-cd $curdir
-echo "All done."
+if [[ $dirs != '' ]]
+then
+    echo "Using target directory tree: $target"
+    for dir in $dirs
+    do
+        echo "--> Updating: $dir"
+        cd $dir
+        git pull origin $(current_branch)
+        echo
+        cd $curdir
+    done
+else
+    echo "No git repositories found on: $target"
+    echo "Usage: $0 directory-tree"
+    exit 1
+fi
 
 exit 0
