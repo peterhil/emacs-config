37dd44804b9b71919faa3954fa7ae7ea783df0b3
Peter Hillerström <peter.hillerstrom@gmail.com>
2015-06-28 22:34:22 +0300

    Move web-mode config into web.el from html-markup.el.

--- rc/html-markup.el
+++ rc/html-markup.el
@@ -83,35 +83,3 @@
     (make-local-variable 'sgml-basic-offset)
     (setq-default sgml-basic-offset 4)
     (sgml-guess-indent)))
-
-;; ==============================================================================
-;; Web mode -- https://github.com/fxbois/web-mode
-;; ------------------------------------------------------------------------------
-
-(require 'web-mode)
-(add-to-list 'magic-mode-alist '("\\(?:<\\?xml\\s +[^>]*>\\)?\\s *<\\(?:!--\\(?:[^-]\\|-[^-]\\)*-->\\s *<\\)*\\(?:!DOCTYPE\\s +[^>]*>\\s *<\\s *\\(?:!--\\(?:[^-]\\|-[^-]\\)*-->\\s *\<\\)*\\)?[Hh][Tt][Mm][Ll]" . web-mode))
-
-(add-to-list 'auto-mode-alist '("\\.html\\?\\'" . web-mode))
-
-;; (setq auto-mode-alist
-;;       (append
-;;        '(
-;;          ("\\.htm\\'" . web-mode)
-;;          ("\\.html\\'" . web-mode)
-;;          ("\\.xhtml\\'" . web-mode)
-;;          ("\\.erb\\'" . web-mode)
-;;          ("\\.mustache\\'" . web-mode)
-;;          ("\\.php\\'" . web-mode)
-;;          ("\\.ctp\\'" . web-mode)
-;;          )
-;;        auto-mode-alist))
-
-; The matching is done on the path. You can alo configure like this if your templates are stored in a subdirectory called views, html or templates.
-(add-to-list 'auto-mode-alist '("/\\(views\\|html\\|templates\\)/.*\\.\\(php\\|twig\\)\\'" . web-mode))
-
-;; Custom coding standards
-(add-hook 'web-mode-hook
-          (lambda ()
-            (setq indent-tabs-mode nil)
-            (setq tab-width (default-value 'tab-width))
-            ))
---
+++ rc/web.el
@@ -0,0 +1,31 @@
+;; ==============================================================================
+;; Web mode -- https://github.com/fxbois/web-mode
+;; ------------------------------------------------------------------------------
+
+(require 'web-mode)
+(add-to-list 'magic-mode-alist '("\\(?:<\\?xml\\s +[^>]*>\\)?\\s *<\\(?:!--\\(?:[^-]\\|-[^-]\\)*-->\\s *<\\)*\\(?:!DOCTYPE\\s +[^>]*>\\s *<\\s *\\(?:!--\\(?:[^-]\\|-[^-]\\)*-->\\s *\<\\)*\\)?[Hh][Tt][Mm][Ll]" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.html\\?\\'" . web-mode))
+
+;; (setq auto-mode-alist
+;;       (append
+;;        '(
+;;          ("\\.htm\\'" . web-mode)
+;;          ("\\.html\\'" . web-mode)
+;;          ("\\.xhtml\\'" . web-mode)
+;;          ("\\.erb\\'" . web-mode)
+;;          ("\\.mustache\\'" . web-mode)
+;;          ("\\.php\\'" . web-mode)
+;;          ("\\.ctp\\'" . web-mode)
+;;          )
+;;        auto-mode-alist))
+
+; The matching is done on the path. You can alo configure like this if your templates are stored in a subdirectory called views, html or templates.
+(add-to-list 'auto-mode-alist '("/\\(views\\|html\\|templates\\)/.*\\.\\(php\\|twig\\)\\'" . web-mode))
+
+;; Custom coding standards
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (setq tab-width (default-value 'tab-width))
+            ))
