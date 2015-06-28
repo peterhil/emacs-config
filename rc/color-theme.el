5f8112be9cac2a26792b041728dc5b8bc17775c3
Peter Hillerström <peter.hillerstrom@gmail.com>
2015-06-28 23:17:59 +0300

    Re-enable disabled color-theme.el parts.

--- rc/color-theme.el
+++ rc/color-theme.el
@@ -8,14 +8,6 @@
 (color-theme-initialize)
 (setq color-theme-is-global t)
 
-;; Set themes
-;; -----------------------------------------------------------------------------
-(if (> (display-color-cells) 256)
-  (progn
-    (require 'color-theme-monokai)
-    (color-theme-monokai))
-  (color-theme-pok-wob))
-
 ;; Mode specific themes
 ;; -----------------------------------------------------------------------------
 (require 'color-theme-buffer-local)
@@ -24,3 +16,20 @@
             (progn
               (color-theme-buffer-local 'color-theme-jsc-dark (current-buffer))
               (buffer-face-toggle))))
+
+;; Set themes
+;; -----------------------------------------------------------------------------
+(if (> (display-color-cells) 256)
+  (progn
+    (require 'color-theme-monokai)
+    (color-theme-monokai))
+  (color-theme-pok-wob))
+
+;; Themes form Melpa
+;; -----------------------------------------------------------------------------
+;; (if (> (display-color-cells) 256)
+;;     (load-theme 'monokai t)
+;;   (load-theme 'jazz t))
+
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/color-theme-mgl")
+;; (load-theme 'mgl-dark t)
