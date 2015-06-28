2c8977fb5ffe98f74d8f3afc6146a0761857a0dc
Peter Hillerström <peter.hillerstrom@gmail.com>
2015-06-28 23:23:24 +0300

    Start replacing manually installed modes with package.el configuration that auto-installs missing packages.

--- rc/packages.el
+++ rc/packages.el
@@ -5,13 +5,14 @@
 (require 'package)
 
 (when (< emacs-major-version 24)
-  (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/"))))
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (package-initialize)
+
 
 (defun package-update-load-path ()
   "Update the load path for newly installed packages."
@@ -31,3 +32,42 @@
               (setq path (format "%s/%s-%s" package-dir stem version))
               (add-to-list 'load-path path)))
           package-alist)))
+
+
+;; Auto-download archive if missing
+(when (not package-archive-contents) (package-refresh-contents))
+
+;; Auto-download missing packages
+(defvar my-packages '(
+                      ace-jump-mode
+                      ack-and-a-half
+                      magit
+                      move-text
+                      org
+                      ruby-compilation
+                      smartparens
+                      clojure-mode
+                      coffee-mode
+                      flycheck
+                      js2-mode
+                      markdown-mode
+                      haml-mode
+                      paredit
+                      grizzl
+                      projectile
+                      rainbow-mode
+                      web-mode
+                      yaml-mode
+                      less-css-mode
+                      slime
+                      php-mode
+                      )
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; (package-install 'pure)
+
+(provide 'packages)
