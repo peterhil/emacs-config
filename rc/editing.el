614b08cf4a714bb23acd32d536114a35a376f31a
Peter Hillerström <peter.hillerstrom@gmail.com>
2015-10-19 14:56:00 +0300

    Associate ERB and JSX files with web-mode.

--- rc/editing.el
+++ rc/editing.el
@@ -80,6 +80,20 @@
 (global-set-key (kbd "<f5>") 'copy-buffer-dir)
 
 
+;; Disable yas tab key
+
+;; It is crucial you first activate yasnippet's global mode.
+;; (yas/global-mode 1)
+
+;; This illustrates how to redefine yas-expand to S-TAB.
+;; (define-key yas-minor-mode-map [backtab]     'yas-expand)
+
+;; Strangely, just redefining one of the variations below won't work.
+;; All rebinds seem to be needed.
+;; (define-key yas-minor-mode-map [(tab)]        nil)
+;; (define-key yas-minor-mode-map (kbd "TAB")    nil)
+;; (define-key yas-minor-mode-map (kbd "<tab>")  nil)
+
 ;; ==============================================================================
 ;; Fuzzy format (auto select tabs or spaces for indent)
 ;; ------------------------------------------------------------------------------
