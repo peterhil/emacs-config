9e240dfbeedb73feb1b64c75d0427cc314fcb160
Peter Hillerström <peter.hillerstrom@gmail.com>
2015-10-27 14:57:35 +0200

    Some custom colours added to custom.el.

--- rc/custom.el
+++ rc/custom.el
@@ -7,6 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#222627" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 307 t)
  '(aquamacs-tool-bar-user-customization nil t)
@@ -21,11 +23,12 @@
  '(c-special-indent-hook (quote (ignore)))
  '(c-tab-always-indent (quote other))
  '(color-theme-is-cumulative t)
+ '(compilation-message-face (quote default))
  '(create-frame-min-width 120)
  '(current-language-environment "UTF-8")
  '(custom-safe-themes
    (quote
-    ("4ff23437b3166eeb7ca9fa026b2b030bba7c0dfdc1ff94df14dfb1bcaee56c78" "49ad7c8d458074db7392f8b8a49235496e9228eb2fa6d3ca3a7aa9d23454efc6" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default)))
+    ("c1019c5b8467d5384cfc33faa90a52fa5033b0b6224f2e6871c5cc70f752f79c" "4ff23437b3166eeb7ca9fa026b2b030bba7c0dfdc1ff94df14dfb1bcaee56c78" "49ad7c8d458074db7392f8b8a49235496e9228eb2fa6d3ca3a7aa9d23454efc6" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default)))
  '(debug-on-error t)
  '(default-frame-alist
     (quote
@@ -39,9 +42,7 @@
       (background-color . "#272822")
       (background-mode . dark)
       (cursor-color . "#73d216")
-      (mouse-color . "#73d216")
-      (left-fringe . -1)
-      (right-fringe . -1))))
+      (mouse-color . "#73d216"))))
  '(default-input-method "finnish-keyboard")
  '(dired-auto-revert-buffer (quote dired-directory-changed-p))
  '(dired-use-ls-dired nil)
@@ -51,12 +52,25 @@
  '(erc-modules
    (quote
     (autojoin button completion fill irccontrols keep-place list log match menu move-to-prompt netsplit networks noncommands readonly ring services stamp track)))
+ '(fci-rule-color "#4E5959")
  '(fill-column 78)
  '(fringe-mode (quote (nil . 0)) nil (fringe))
  '(global-hl-line-mode t)
  '(global-linum-mode t)
+ '(highlight-changes-colors ("#FD5FF0" "#AE81FF"))
+ '(highlight-tail-colors
+   (quote
+    (("#4E5959" . 0)
+     ("#67930F" . 20)
+     ("#349B8D" . 30)
+     ("#21889B" . 50)
+     ("#968B26" . 60)
+     ("#A45E0A" . 70)
+     ("#A41F99" . 85)
+     ("#4E5959" . 100))))
  '(indent-tabs-mode nil)
  '(indicate-buffer-boundaries (quote left))
+ '(magit-diff-use-overlays nil)
  '(mweb-submode-indent-offset 4)
  '(ns-tool-bar-display-mode (quote labels) t)
  '(ns-tool-bar-size-mode (quote regular) t)
@@ -79,10 +93,34 @@
  '(tab-width 4)
  '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
  '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
  '(visual-line-mode nil t)
  '(web-mode-code-indent-offset 4)
  '(web-mode-css-indent-offset 4)
  '(web-mode-markup-indent-offset 4)
+ '(weechat-color-list
+   (unspecified "#222627" "#4E5959" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))
  '(x-select-enable-clipboard t)
  '(yaml-indent-offset 4))
 
