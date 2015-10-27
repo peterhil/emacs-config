0216ae0f7448f28cd556266b1e0f2519edabfb74
Peter Hillerström <peter.hillerstrom@gmail.com>
2015-10-27 14:55:12 +0200

    Fix autoloading markdown-mode.

--- rc/markdown.el
+++ rc/markdown.el
@@ -2,7 +2,7 @@
 ;; Markdown
 ;; ------------------------------------------------------------------------------
 
-(autoload 'markdown-mode "Markdown mode" "Major mode for editing Markdown files" t)
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.MD$" . markdown-mode))
