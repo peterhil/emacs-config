fdcbf257a49c67dd97667c7bb807a448adaf561f
Peter Hillerström <peter.hillerstrom@gmail.com>
2017-09-25 10:23:50 +0300

    Configure git modes with use-package

--- rc/git.el
+++ rc/git.el
@@ -2,25 +2,17 @@
 ;; Git support
 ;; -----------------------------------------------------------------------------
 
-(cond ((eq system-type 'linux)
-       (progn
-         (add-to-list 'load-path (concat usr-prefix "/share/git-core/emacs/"))
-         (add-to-list 'load-path (concat usr-prefix "/share/emacs/23.2/lisp/"))))
-      ((eq system-type 'darwin)
-       (progn
-         (if (command-which-matches "git" ".nix-profile")
-             (add-to-list 'load-path "~/.nix-profile/share/git/contrib/emacs/")
-           (add-to-list 'load-path (concat usr-prefix "/share/git-core/contrib/emacs/")))
-         (if (command-which-matches "emacs" ".nix-profile")
-             (add-to-list 'load-path "~/.nix-profile/share/emacs/24.5/lisp/vc/")
-           (add-to-list 'load-path (concat usr-prefix "/usr/local/share/emacs/23.3/lisp/")))
-         )))
+(use-package "git")
+(use-package "git-blame")
+(use-package "vc-git")
 
-(require 'git)
-; (require 'git-blame)
-; (require 'vc-git)
-
-(add-to-list 'vc-handled-backends 'GIT)
-
-(setq magit-last-seen-setup-instructions "1.4.0")
-(setq magit-diff-use-overlays nil)
+;; Magit git client in Emacs: https://magit.vc/manual/magit/index.html
+(use-package "magit"
+  :disabled
+  :init
+  ;; Disable auto revert mode, and suppress annoying message about the feature.
+  ;; See background: https://github.com/magit/magit/issues/1803
+  (setq magit-last-seen-setup-instructions "1.4.0")
+  (setq magit-auto-revert-mode nil)
+  (setq magit-diff-use-overlays nil)
+  )
