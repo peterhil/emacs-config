346252297d57cc6bbe33d0ac186d239285aae2d7
Peter Hillerström <peter.hillerstrom@gmail.com>
2017-09-26 20:47:02 +0300

    Use my-auto-mode-extensions on rainbow-mode config

--- rc/rainbow-mode.el
+++ rc/rainbow-mode.el
@@ -1,9 +1,17 @@
-(require 'rainbow-mode)
+;; ==============================================================================
+;; Rainbow mode
+;; ------------------------------------------------------------------------------
+;;
+;; https://julien.danjou.info/projects/emacs-packages#rainbow-mode
 
+(require 'rainbow-mode)
 (autoload 'rainbow-mode "rainbow-mode.el" "Minor mode for editing HTML colors" t)
 
-(add-to-list 'auto-mode-alist '("\\.html$" . rainbow-mode))
-(add-to-list 'auto-mode-alist '("\\.less$" . rainbow-mode))
-(add-to-list 'auto-mode-alist '("\\.css$" . rainbow-mode))
-(add-to-list 'auto-mode-alist '("\\.coffee$" . rainbow-mode))
-(add-to-list 'auto-mode-alist '("\\.js$" . rainbow-mode))
+(my-auto-mode-extensions
+ 'rainbow-mode
+ '("css"
+   "html"
+   "less"
+   "sass"
+   "scss"
+   "styl"))
