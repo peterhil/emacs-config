48488cee8f96500ce83c7e25cd0e0d3d9e54d5a0
Peter Hillerström <peter.hillerstrom@gmail.com>
2018-10-09 19:20:57 +0200

    Disable Marmalade package repository, because it is not maintained securely

--- rc/packages.el
+++ rc/packages.el
@@ -8,8 +8,8 @@
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
 
 (package-initialize)
 
