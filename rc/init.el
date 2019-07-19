2e46917d328dcd6aba44f623f4e8ed1e3f22aac3
Peter Hillerström <peter.hillerstrom@gmail.com>
2019-07-20 00:43:59 +0300

    init.el: Enable flymake.el

--- rc/init.el
+++ rc/init.el
@@ -43,7 +43,8 @@
            ;; "jira"
            ;; "qt"
            ;; "projectile"
-           ;; "flymake"
+           ;; "flycheck
+           "flymake"
 
            ;; -- Markup
            ;; "org"
