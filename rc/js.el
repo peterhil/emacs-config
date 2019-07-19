993ebbd4df247cbfcc50944902c548f313c08513
Peter Hillerström <peter.hillerstrom@gmail.com>
2019-07-19 22:44:23 +0300

    js.el: Replace js2-mode with more current javascript-mode

--- rc/js.el
+++ rc/js.el
@@ -2,17 +2,20 @@
 ;; Javascript
 ;; ------------------------------------------------------------------------------
 
-(require 'js2-mode)
-(autoload 'js2-mode "js2-mode" nil t)
+;; (require 'javascript-mode)
+(autoload 'javascript-mode "javascript-mode" nil t)
 
 ;; https://stackoverflow.com/questions/35162106/how-to-disable-js2-mode-syntax-checking-globally-in-spacemacs
 (setq js2-strict-missing-semi-warning nil)
 
 (my-auto-mode-extensions
- 'js2-mode
- '("js"))
+ 'javascript-mode
+ '(
+   "js"
+   "ts"
+   ))
 
-(add-hook 'js2-mode-hook
+(add-hook 'javascript-mode-hook
           '(lambda()
              (set (make-local-variable 'electric-indent-chars) nil)))
 
