15681bf7129bdb150fd6ca06d90d6890f5b2116b
Peter Hillerström <peter.hillerstrom@gmail.com>
2019-12-25 19:28:53 +0200

    Add auto-minor-mode and make rainbow-mode work automatically

--- rc/custom.el
+++ rc/custom.el
@@ -66,10 +66,18 @@
  '(ns-tool-bar-size-mode (quote regular) t)
  '(package-selected-packages
    (quote
-    (picolisp-mode lua-mode flymake-jslint visual-regexp auto-complete caml tuareg feature-mode vue-html-mode vue-mode typescript-mode xref-js2 js2-refactor zencoding-mode eslint-fix exec-path-from-shell elm-mode csv-nav php-auto-yasnippets zonokai-theme yaml-mode web-mode w3m vmd-mode uuidgen stylus-mode spacegray-theme smartparens slime scss-mode sass-mode ruby-compilation rainbow-mode qml-mode python-mode projectile php-mode php-completion paredit move-text monokai-theme mkdown md-readme markdown-mode+ magit linum-off less-css-mode json-mode js2-mode jinja2-mode jazz-theme jade-mode hlinum grizzl fuzzy-format flymd flymake-php flymake-json flymake-css flymake-coffee flycheck-pyflakes flycheck-haskell flycheck-clojure erlang elixir-mix editorconfig cypher-mode color-theme-sanityinc-tomorrow coffee-mode anything alchemist ag ack-and-a-half ace-jump-mode ac-dabbrev)))
+    (auto-minor-mode picolisp-mode lua-mode flymake-jslint visual-regexp auto-complete caml tuareg feature-mode vue-html-mode vue-mode typescript-mode xref-js2 js2-refactor zencoding-mode eslint-fix exec-path-from-shell elm-mode csv-nav php-auto-yasnippets zonokai-theme yaml-mode web-mode w3m vmd-mode uuidgen stylus-mode spacegray-theme smartparens slime scss-mode sass-mode ruby-compilation rainbow-mode qml-mode python-mode projectile php-mode php-completion paredit move-text monokai-theme mkdown md-readme markdown-mode+ magit linum-off less-css-mode json-mode js2-mode jinja2-mode jazz-theme jade-mode hlinum grizzl fuzzy-format flymd flymake-php flymake-json flymake-css flymake-coffee flycheck-pyflakes flycheck-haskell flycheck-clojure erlang elixir-mix editorconfig cypher-mode color-theme-sanityinc-tomorrow coffee-mode anything alchemist ag ack-and-a-half ace-jump-mode ac-dabbrev)))
  '(safe-local-variable-values
    (quote
-    ((Syntax . ANSI-Common-Lisp)
+    ((eval when
+           (fboundp
+            (quote aggressive-indent-mode))
+           (aggressive-indent-mode -1))
+     (eval when
+           (fboundp
+            (quote rainbow-mode))
+           (rainbow-mode 1))
+     (Syntax . ANSI-Common-Lisp)
      (syntax . ANSI-Common-Lisp))))
  '(save-place-mode t nil (saveplace))
  '(select-enable-clipboard t)
