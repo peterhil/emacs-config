9f854c55bd90114ae791ff6167e739a6109dd849
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-24 20:35:35 +0300

    New customizations by Aquamacs 3.5

--- rc/custom.el
+++ rc/custom.el
@@ -3,118 +3,127 @@
 ;; ------------------------------------------------------------------------------
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(ansi-color-names-vector
-     ["#222627" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
-  '(anything-command-map-prefix-key "C-x a")
-  '(aquamacs-additional-fontsets nil t)
-  '(aquamacs-customization-version-id 308 t)
-  '(aquamacs-tool-bar-user-customization nil t)
-  '(better-fringes-mode t)
-  '(c-echo-syntactic-information-p t)
-  '(c-special-indent-hook '(ignore))
-  '(c-tab-always-indent 'other)
-  '(color-theme-is-cumulative nil)
-  '(compilation-message-face 'default)
-  '(create-frame-min-width 120)
-  '(current-language-environment "UTF-8")
-  '(custom-safe-themes
-     '("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default))
-  '(debug-on-error t)
-  '(default-frame-alist
-     '((font-backend ns)
-        (fontsize . 0)
-        (menu-bar-lines . 1)
-        (tool-bar-lines . 0)
-        (border-color . "#0f0f0f")
-        (modeline . t)
-        (foreground-color . "#F8F8F2")
-        (background-color . "#222222")
-        (background-mode . dark)
-        (cursor-color . "#73d216")
-        (mouse-color . "#73d216")))
-  '(default-input-method "finnish-keyboard")
-  '(desktop-save-mode nil)
-  '(dired-auto-revert-buffer 'dired-directory-changed-p)
-  '(dired-use-ls-dired t)
-  '(emulate-mac-finnish-keyboard-mode t)
-  '(fci-rule-color "#4E5959")
-  '(fill-column 78)
-  '(font-use-system-font t)
-  '(fringe-mode '(nil . 0) nil (fringe))
-  '(global-hl-line-mode t)
-  '(global-linum-mode t)
-  '(highlight-changes-colors ("#FD5FF0" "#AE81FF"))
-  '(highlight-tail-colors
-     '(("#4E5959" . 0)
-        ("#67930F" . 20)
-        ("#349B8D" . 30)
-        ("#21889B" . 50)
-        ("#968B26" . 60)
-        ("#A45E0A" . 70)
-        ("#A41F99" . 85)
-        ("#4E5959" . 100)))
-  '(indent-tabs-mode nil)
-  '(indicate-buffer-boundaries 'left)
-  '(inhibit-startup-screen t)
-  '(ns-tool-bar-display-mode 'labels t)
-  '(ns-tool-bar-size-mode 'regular t)
-  '(package-selected-packages
-     '(fortune-cookie form-feed magit-delta fireplace use-package d-mode auto-minor-mode picolisp-mode lua-mode flymake-jslint visual-regexp auto-complete caml tuareg feature-mode vue-html-mode vue-mode typescript-mode xref-js2 zencoding-mode eslint-fix exec-path-from-shell elm-mode csv-nav php-auto-yasnippets zonokai-theme yaml-mode web-mode w3m vmd-mode uuidgen stylus-mode spacegray-theme smartparens slime scss-mode sass-mode ruby-compilation rainbow-mode qml-mode python-mode projectile php-mode php-completion paredit move-text monokai-theme mkdown md-readme markdown-mode+ linum-off less-css-mode json-mode jinja2-mode jazz-theme jade-mode hlinum grizzl fuzzy-format flymd flymake-php flymake-json flymake-css flymake-coffee flycheck-pyflakes flycheck-haskell flycheck-clojure erlang elixir-mix editorconfig cypher-mode color-theme-sanityinc-tomorrow coffee-mode anything alchemist ag ack-and-a-half ace-jump-mode ac-dabbrev))
-  '(safe-local-variable-values
-     '((eval when
-         (fboundp 'aggressive-indent-mode)
-         (aggressive-indent-mode -1))
-        (eval when
-          (fboundp 'rainbow-mode)
-          (rainbow-mode 1))
-        (Syntax . ANSI-Common-Lisp)
-        (syntax . ANSI-Common-Lisp)))
-  '(save-place-mode t nil (saveplace))
-  '(select-enable-clipboard t)
-  '(semantic-stickyfunc-indent-string " ")
-  '(show-paren-mode t)
-  '(show-trailing-whitespace t)
-  '(size-indication-mode t)
-  '(split-width-threshold 240)
-  '(tab-always-indent t)
-  '(tab-width 4)
-  '(text-mode-hook '(turn-on-auto-fill text-mode-hook-identify))
-  '(tool-bar-mode nil)
-  '(uniquify-buffer-name-style 'forward nil (uniquify))
-  '(vc-annotate-background nil)
-  '(vc-annotate-color-map
-     '((20 . "#F92672")
-        (40 . "#CF4F1F")
-        (60 . "#C26C0F")
-        (80 . "#E6DB74")
-        (100 . "#AB8C00")
-        (120 . "#A18F00")
-        (140 . "#989200")
-        (160 . "#8E9500")
-        (180 . "#A6E22E")
-        (200 . "#729A1E")
-        (220 . "#609C3C")
-        (240 . "#4E9D5B")
-        (260 . "#3C9F79")
-        (280 . "#A1EFE4")
-        (300 . "#299BA6")
-        (320 . "#2896B5")
-        (340 . "#2790C3")
-        (360 . "#66D9EF")))
-  '(vc-annotate-very-old-color nil)
-  '(visual-line-mode nil t)
-  '(weechat-color-list
-     (unspecified "#222627" "#4E5959" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))
-  '(yaml-indent-offset 4))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#222627" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(anything-command-map-prefix-key "C-x a")
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 312 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(better-fringes-mode t)
+ '(c-echo-syntactic-information-p t)
+ '(c-special-indent-hook (quote (ignore)))
+ '(c-tab-always-indent (quote other))
+ '(color-theme-is-cumulative nil)
+ '(compilation-message-face (quote default))
+ '(create-frame-min-width 120)
+ '(current-language-environment "UTF-8")
+ '(custom-safe-themes
+   (quote
+    ("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
+ '(debug-on-error t)
+ '(default-frame-alist
+    (quote
+     ((border-color . "#0f0f0f")
+      (mouse-color . "#73d216")
+      (cursor-color . "#73d216")
+      (modeline . t)
+      (background-mode . dark)
+      (tool-bar-lines . 0)
+      (menu-bar-lines . 1)
+      (background-color . "#222222")
+      (foreground-color . "#F8F8F2")
+      (font . "-*-Menlo-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+      (fontsize . 0)
+      (font-backend mac-ct))))
+ '(default-input-method "finnish-keyboard")
+ '(desktop-save-mode nil)
+ '(dired-auto-revert-buffer (quote dired-directory-changed-p))
+ '(dired-use-ls-dired t)
+ '(emulate-mac-finnish-keyboard-mode t)
+ '(fci-rule-color "#4E5959")
+ '(fill-column 78)
+ '(font-use-system-font t)
+ '(fringe-mode (quote (nil . 0)) nil (fringe))
+ '(global-hl-line-mode t)
+ '(global-linum-mode t)
+ '(highlight-changes-colors ("#FD5FF0" "#AE81FF"))
+ '(highlight-tail-colors
+   (quote
+    (("#4E5959" . 0)
+     ("#67930F" . 20)
+     ("#349B8D" . 30)
+     ("#21889B" . 50)
+     ("#968B26" . 60)
+     ("#A45E0A" . 70)
+     ("#A41F99" . 85)
+     ("#4E5959" . 100))))
+ '(indent-tabs-mode nil)
+ '(indicate-buffer-boundaries (quote left))
+ '(inhibit-startup-screen t)
+ '(ns-tool-bar-display-mode (quote labels) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(one-buffer-one-frame-mode t nil (aquamacs-frame-setup))
+ '(package-selected-packages
+   (quote
+    (utop merlin fill-column-indicator faustine faust-mode fortune-cookie form-feed magit-delta fireplace use-package d-mode auto-minor-mode picolisp-mode lua-mode flymake-jslint visual-regexp auto-complete caml tuareg vue-html-mode vue-mode typescript-mode xref-js2 zencoding-mode eslint-fix exec-path-from-shell elm-mode csv-nav php-auto-yasnippets zonokai-theme yaml-mode web-mode w3m vmd-mode uuidgen stylus-mode spacegray-theme smartparens slime scss-mode sass-mode ruby-compilation rainbow-mode qml-mode python-mode projectile php-completion paredit move-text monokai-theme mkdown md-readme markdown-mode+ linum-off json-mode jinja2-mode jazz-theme jade-mode hlinum grizzl fuzzy-format flymd flymake-php flymake-json flymake-css flymake-coffee flycheck-pyflakes flycheck-haskell flycheck-clojure erlang elixir-mix editorconfig cypher-mode color-theme-sanityinc-tomorrow coffee-mode anything alchemist ag ack-and-a-half ace-jump-mode ac-dabbrev)))
+ '(safe-local-variable-values
+   (quote
+    ((eval when
+           (fboundp
+            (quote aggressive-indent-mode))
+           (aggressive-indent-mode -1))
+     (eval when
+           (fboundp
+            (quote rainbow-mode))
+           (rainbow-mode 1))
+     (Syntax . ANSI-Common-Lisp)
+     (syntax . ANSI-Common-Lisp))))
+ '(save-place-mode t nil (saveplace))
+ '(select-enable-clipboard t)
+ '(semantic-stickyfunc-indent-string " ")
+ '(show-trailing-whitespace t)
+ '(size-indication-mode t)
+ '(split-width-threshold 240)
+ '(tab-always-indent t)
+ '(tab-width 4)
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(visual-line-mode nil t)
+ '(weechat-color-list
+   (unspecified "#222627" "#4E5959" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))
+ '(yaml-indent-offset 4))
 
 
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
