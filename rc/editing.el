d5a220987c153572d106eef142adfd9047b33c2f
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-25 20:04:46 +0300

    Fix editorconfig and smart-tabs config by not using requires keyword

--- rc/editing.el
+++ rc/editing.el
@@ -5,13 +5,12 @@
 (use-package "editorconfig"
   ;; Read EditorConfig files (.editorconfig) -- http://editorconfig.org/
   ;; https://github.com/editorconfig/editorconfig-emacs.git
-  :requires editorconfig
   :config
-  (progn
-    (editorconfig-mode 1)
-    ;; ignore indent_size on lisp modes
-    (setq editorconfig-lisp-use-default-indent t)
-  ))
+  (editorconfig-mode 1)
+
+  ;; Ignore indent_size on lisp modes
+  (setq editorconfig-lisp-use-default-indent t)
+  )
 
 
 ;; ==============================================================================
--- rc/smart-tabs.el
+++ rc/smart-tabs.el
@@ -3,9 +3,45 @@
 ;; http://www.emacswiki.org/SmartTabs
 ;; ------------------------------------------------------------------------------
 
+(defadvice align (around smart-tabs activate)
+  (let ((indent-tabs-mode indent-tabs-mode)) ad-do-it))
+
+(defadvice align-regexp (around smart-tabs activate)
+  (let ((indent-tabs-mode indent-tabs-mode)) ad-do-it))
+
+(defadvice indent-relative (around smart-tabs activate)
+  (let ((indent-tabs-mode indent-tabs-mode)) ad-do-it))
+
+(defadvice indent-according-to-mode (around smart-tabs activate)
+  (let ((indent-tabs-mode indent-tabs-mode))
+    (if (memq indent-line-function
+              '(indent-relative
+                indent-relative-maybe))
+        (setq indent-tabs-mode indent-tabs-mode))
+    ad-do-it))
+
+(defmacro smart-tabs-advice (function offset)
+  `(progn
+     (defvaralias ',offset 'tab-width)
+     (defadvice ,function (around smart-tabs activate)
+       (cond
+        (indent-tabs-mode
+         (save-excursion
+           (beginning-of-line)
+           (while (looking-at "\t*\\( +\\)\t+")
+             (replace-match "" nil nil nil 1)))
+         (setq tab-width tab-width)
+         (let ((tab-width fill-column)
+               (,offset fill-column)
+               (wstart (window-start)))
+           (unwind-protect
+               (progn ad-do-it)
+             (set-window-start (selected-window) wstart))))
+        (t
+         ad-do-it)))))
+
 (use-package "smart-tab"
-  :requires 'cl-lib
-  :defer t
+  :load-path "site-lisp/smart-tab"
   :config
   (global-smart-tab-mode t)
   ;; (global-set-key '[C-tab] 'smart-tab)
@@ -19,46 +55,7 @@
 
   (make-local-variable 'tab-width)
   (setq tab-width (or (default-value 'tab-width) 4))
-
   (setq cua-auto-tabify-rectangles nil)
-
-  (defadvice align (around smart-tabs activate)
-    (let ((indent-tabs-mode indent-tabs-mode)) ad-do-it))
-
-  (defadvice align-regexp (around smart-tabs activate)
-    (let ((indent-tabs-mode indent-tabs-mode)) ad-do-it))
-
-  (defadvice indent-relative (around smart-tabs activate)
-    (let ((indent-tabs-mode indent-tabs-mode)) ad-do-it))
-
-  (defadvice indent-according-to-mode (around smart-tabs activate)
-    (let ((indent-tabs-mode indent-tabs-mode))
-      (if (memq indent-line-function
-                '(indent-relative
-                  indent-relative-maybe))
-          (setq indent-tabs-mode indent-tabs-mode))
-      ad-do-it))
-
-  (defmacro smart-tabs-advice (function offset)
-    `(progn
-       (defvaralias ',offset 'tab-width)
-       (defadvice ,function (around smart-tabs activate)
-         (cond
-          (indent-tabs-mode
-           (save-excursion
-             (beginning-of-line)
-             (while (looking-at "\t*\\( +\\)\t+")
-               (replace-match "" nil nil nil 1)))
-           (setq tab-width tab-width)
-           (let ((tab-width fill-column)
-                 (,offset fill-column)
-                 (wstart (window-start)))
-             (unwind-protect
-                 (progn ad-do-it)
-               (set-window-start (selected-window) wstart))))
-          (t
-           ad-do-it)))))
-
   (smart-tabs-advice c-indent-line c-basic-offset)
   (smart-tabs-advice c-indent-region c-basic-offset)
 
