298ae9d12acb257526ca0d6d405a5e7bfa66a99b
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-25 20:11:52 +0300

    Document use-package options better

--- rc/init.el
+++ rc/init.el
@@ -55,17 +55,15 @@
 ;; (add-to-list 'load-path (concat user-emacs-directory "site-lisp/use-package"))
 (require 'use-package)
 (setq
+  ;; Make use-package always install missing packages with use-package-always-ensure
+  ;; https://github.com/jwiegley/use-package#installing-use-package
+  ;; Note! Incompatible with `straight-use-package-by-default`!
   use-package-always-ensure t
+
+  ;; Pinning versions by giving default value for :pin in each use-package:
+  ;; use-package-always-pin "melpa"
+
+  ;; Debugging use-package
   use-package-verbose 'errors
   ;; use-package-expand-minimally t
   )
-
-;; Make use-package always install missing packages
-;; https://github.com/jwiegley/use-package#installing-use-package
-;; Note! Incompatible with `straight-use-package-by-default`!
-
-;; (require 'use-package-ensure)
-;; (setq use-package-always-ensure t)
-
-;; Default value for :pin in each use-package.
-;; (setq use-package-always-pin "melpa")
