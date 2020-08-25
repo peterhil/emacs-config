f388b1d9e887d12e52697b4f7691ffa2a6f4b5a5
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-25 14:19:15 +0300

    Use-package on rainbow-mode.el

--- rc/rainbow-mode.el
+++ rc/rainbow-mode.el
@@ -4,16 +4,16 @@
 ;;
 ;; https://julien.danjou.info/projects/emacs-packages#rainbow-mode
 
-(require 'rainbow-mode)
-(autoload 'rainbow-mode "rainbow-mode.el" "Minor mode for editing HTML colors" t)
-
-(my-auto-minor-mode-extensions
- 'rainbow-mode
- '("css"
-   "html"
-   "less"
-   "sass"
-   "scss"
-   "styl"))
-
-(add-to-list 'auto-minor-mode-alist '("-theme\\.el\\'" . rainbow-mode))
+(use-package "rainbow-mode"
+  ;; (autoload 'rainbow-mode "rainbow-mode.el" "Minor mode for editing HTML colors" t)
+  :mode
+  (
+    "\\.css\\'"
+    "\\.html\\'"
+    "\\.less\\'"
+    "\\.sass\\'"
+    "\\.scss\\'"
+    "\\.styl\\'"
+    "-theme\\.el\\'"
+    )
+  )
