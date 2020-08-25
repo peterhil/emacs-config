ce23159bdc8a47a4aa003b60332987075c579e8b
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-25 15:43:17 +0300

    Use-package on pure.el

--- rc/pure.el
+++ rc/pure.el
@@ -3,14 +3,17 @@
 ;; ------------------------------------------------------------------------------
 
 ;; HideShow support
-;; (require 'hideshow)
-;; (add-hook 'pure-mode-hook 'hs-minor-mode)
+;; (use-package "hideshow"
+;;   :hook (pure-mode . 'hs-minor-mode))
 
 ;; Pure documentation browsing with w3m
-;; (require 'w3m-load)
+;; (use-package "w3m-load")
 
-(require 'pure-mode)
-(add-to-list 'auto-mode-alist '("\\.pure\\(rc\\)?\\'" . pure-mode))
-
-(add-hook 'pure-mode-hook 'turn-on-font-lock)
-(add-hook 'pure-eval-mode-hook 'turn-on-font-lock)
+(use-package "pure-mode"
+  :load-path "site-lisp/pure-mode"
+  :mode
+  "\\.pure\\'"
+  "\\.purerc\\'"
+  :hook (
+         (pure-mode . 'turn-on-font-lock)
+         (pure-eval-mode . 'turn-on-font-lock)))
