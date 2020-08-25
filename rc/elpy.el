5315480bc1ee37b0b1d87d33e412066d7a4b4604
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-26 00:08:22 +0300

    Disable elpy-module-highlight-indentation

--- rc/elpy.el
+++ rc/elpy.el
@@ -1,4 +1,16 @@
 (use-package elpy
   :defer t
   :init
-  (advice-add 'python-mode :before 'elpy-enable))
+  (advice-add 'python-mode :before 'elpy-enable)
+  :config
+  (setq elpy-modules
+    '(
+      elpy-module-sane-defaults
+      elpy-module-company
+      elpy-module-eldoc
+      elpy-module-flymake
+      ;; elpy-module-highlight-indentation
+      elpy-module-pyvenv
+      elpy-module-yasnippet
+      elpy-module-django
+      )))
