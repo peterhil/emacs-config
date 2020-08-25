ea2915f83deb2bbf4bd9408d8568621e573be3f9
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-25 20:06:05 +0300

    Add edit-server on emacsclient.el

--- rc/emacsclient.el
+++ rc/emacsclient.el
@@ -1,3 +1,14 @@
+;; =============================================================================
+;; Emacs server and Emacsclient
+;; -----------------------------------------------------------------------------
+
+(use-package edit-server
+  :if window-system
+  :init
+  (add-hook 'after-init-hook 'server-start t)
+  (add-hook 'after-init-hook 'edit-server-start t))
+
+
 ;; Define function to shutdown emacs server instance
 ;; Can be called from shell with: emacsclient -e '(server-shutdown)'
 (defun server-shutdown ()
