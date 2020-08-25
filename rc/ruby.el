1e24275045331295a606f8633ad06c54bdce2180
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-25 15:27:48 +0300

    Use-package on ruby.el

--- rc/ruby.el
+++ rc/ruby.el
@@ -2,24 +2,27 @@
 ;; Ruby
 ;; ------------------------------------------------------------------------------
 
-(require 'ruby-mode)
-
-(my-auto-mode-regexps
- 'ruby-mode
- '("\\.rb\\'"
-   "^Capfile$"))
-
-;; Disable adding coding system magic comment - http://stackoverflow.com/a/6454077
-;; # -*- coding: utf-8 -*-
-(setq ruby-insert-encoding-magic-comment nil)
+(use-package "ruby-mode"
+  :mode
+  ("\\.rb\\'"
+   "\\'Capfile\\'")
+  :config
+  ;; Disable adding coding system magic comment - http://stackoverflow.com/a/6454077
+  ;; # -*- coding: utf-8 -*-
+  (setq ruby-insert-encoding-magic-comment nil))
 
 
 ;; ==============================================================================
 ;; Cucumber
 ;; ------------------------------------------------------------------------------
 
-(require 'feature-mode)
-(add-to-list 'auto-mode-alist '("\\.feature\\'" . feature-mode))
+(use-package "feature-mode"
+  :mode "\\.feature\\'")
 
-(require 'haml-mode)
-(add-to-list 'auto-mode-alist '("\\.hamlc\\?\\'" . haml-mode))
+
+;; ==============================================================================
+;; Haml
+;; ------------------------------------------------------------------------------
+
+(use-package "haml-mode"
+  :mode "\\.hamlc?\\'")
