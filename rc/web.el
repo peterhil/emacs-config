ee4e56fd1833ac8717de9417e529504eae4bbca8
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-25 13:43:29 +0300

    Change web-mode config to use-package form

--- rc/web.el
+++ rc/web.el
@@ -2,39 +2,39 @@
 ;; Web mode -- https://github.com/fxbois/web-mode
 ;; ------------------------------------------------------------------------------
 
-(require 'web-mode)
-
-(add-to-list 'magic-mode-alist '("\\`\\(?:<\\?xml\\s +[^>]*>\\)?\\s *<\\(?:!--\\(?:[^-]\\|-[^-]\\)*-->\\s *<\\)*\\(?:!DOCTYPE\\s +[^>]*>\\s *<\\s *\\(?:!--\\(?:[^-]\\|-[^-]\\)*-->\\s *\<\\)*\\)?[Hh][Tt][Mm][Ll]" . web-mode))
-
-(my-auto-mode-regexps
- 'web-mode
- '("/\\(views\\|html\\|templates\\)/.*\\.\\(php\\|twig\\)\\'" ; Matching can be done on the path
-   "/components/.*\\.js\\'"))
-
-(my-auto-mode-extensions
- 'web-mode
- '(
-   "css"
-   "ctp"
-   "erb"
-   "html\\?"
-   "jsx"
-   "marko"
-   "mustache"
-   "php"
-   "svelte"
-   "tag"
-   "vue"
-   "xhtml"
-   ))
-
-;; Custom coding standards
-(add-hook 'web-mode-hook
-          (lambda ()
-            (setq indent-tabs-mode nil)
-            (setq tab-width (default-value 'tab-width))
-            ))
-
-(setq web-mode-code-indent-offset 4)
-(setq web-mode-css-indent-offset 4)
-(setq web-mode-markup-indent-offset 2)
+(use-package "web-mode"
+  :config
+  (progn
+    (setq web-mode-code-indent-offset 4)
+    (setq web-mode-css-indent-offset 4)
+    (setq web-mode-markup-indent-offset 2)
+    (my-auto-mode-regexps
+      'web-mode
+      '(
+         "/\\(views\\|html\\|templates\\)/.*\\.\\(php\\|twig\\)\\'" ; Matching can be done on the path
+         "/components/.*\\.js\\'")))
+  :magic
+  (
+    "<\\!DOCTYPE"
+    "<\\!doctype"
+    "<\\?xml"
+    "<html"
+    "<svg"
+    )
+  :mode
+  (
+    "css"
+    "ctp"
+    "erb"
+    "htm"
+    "html"
+    "jsx"
+    "marko"
+    "mustache"
+    "php"
+    "svelte"
+    "tag"
+    "vue"
+    "xhtml"
+    )
+  )
