c010e0ee16984e1222d12fb866eb64b9cfc74835
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-25 14:09:49 +0300

    Use-package on style.el

--- rc/style.el
+++ rc/style.el
@@ -2,41 +2,37 @@
 ;; Markup
 ;; ------------------------------------------------------------------------------
 
-(require 'jinja2-mode)
-(my-auto-mode-regexps
- 'jinja2-mode
- '("\\.jinja\\'"
-   ;; Do matching on the path and extension
-   "/templates/.*\\.html\\'"))
-
-;; YAML
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.ya\\?ml$" . yaml-mode))
+;; Jinja2
+(use-package "jinja2-mode"
+  :mode
+  (
+    "\\.jinja\\'"
+    ;; Do matching on the path and extension
+    "/templates/.*\\.html\\'"
+    )
+  )
 
 ;; CSS modes
-(require 'css-mode)
-(add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
-
-;; Less CSS
-(require 'less-css-mode)
-(add-to-list 'auto-mode-alist '("\\.less\\'" . less-css-mode))
-
-;; Stylus
-(require 'stylus-mode)
-(add-to-list 'auto-mode-alist '("\\.styl\\'" . stylus-mode))
-
-;; SASS and SCSS
-; (autoload 'scss-mode "scss-mode")
-(require 'scss-mode)
-(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-
-(setq scss-compile-at-save nil)
-
-;; Stylus
-(require 'sws-mode)
-(add-to-list 'auto-mode-alist '("\\.styl\\'" . sws-mode))
+(use-package "css-mode" :mode "\\.css\\'")
 
 ;; Jade
-(require 'jade-mode)
-(add-to-list 'auto-mode-alist '("\\.jade\\'" . jade-mode))
+(use-package "jade-mode" :mode "\\.jade\\'")
+
+;; Less CSS
+(use-package "less-css-mode" :mode "\\.less\\'")
+
+;; Sass and Scss
+(use-package "scss-mode"
+  :config
+  (setq scss-compile-at-save nil)
+  :mode
+  (
+    ("\\.sass\\'" . sass-mode)
+    ("\\.scss\\'" . scss-mode)
+    ))
+
+;; Stylus
+(use-package "stylus-mode" :mode "\\.styl\\'")
+
+;; YAML
+(use-package "yaml-mode" :mode "\\.ya?ml\\'")
