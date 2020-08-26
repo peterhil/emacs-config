e2042e4a5b7305e0fae320563029de9f6004afd3
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-26 16:08:26 +0300

    Use-package on php.el

--- rc/php.el
+++ rc/php.el
@@ -2,56 +2,56 @@
 ;; PHP mode
 ;; ------------------------------------------------------------------------------
 
-(require 'php-mode)
-(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(defun my-ywb-php-lineup-arglist ()
+  (defun ywb-php-lineup-arglist-intro (langelem)
+    (save-excursion
+      (goto-char (cdr langelem))
+      (vector (+ (current-column) c-basic-offset))))
+  (defun ywb-php-lineup-arglist-close (langelem)
+    (save-excursion
+      (goto-char (cdr langelem))
+      (vector (current-column))))
+  (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+  (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close))
 
-(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.inc\\'" . php-mode))
 
-(add-hook 'php-mode-hook
+(use-package php-mode
+  :config (autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+  :mode ("\\.php\\'" "\\.inc\\'")
+  :hook
+  (
+    (php-mode .
+      (lambda ()
+        ;; (setq indent-tabs-mode nil)
+        ;; (setq tab-width (default-value 'tab-width))
+        (my-ywb-php-lineup-arglist)
+        (define-abbrev php-mode-abbrev-table "ex" "extends")
+        ))))
+
+
+(use-package php-completion
+  :hook php-mode
+  :config (php-completion-mode t)
+  :bind ("C-o" . phpcmp-complete))
+
+
+;; Cooperation with auto-complete.el
+(use-package auto-complete
+  :hook (php-mode .
           (lambda ()
-            (defun ywb-php-lineup-arglist-intro (langelem)
-              (save-excursion
-                (goto-char (cdr langelem))
-                (vector (+ (current-column) c-basic-offset))))
-            (defun ywb-php-lineup-arglist-close (langelem)
-              (save-excursion
-                (goto-char (cdr langelem))
-                (vector (current-column))))
-            (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
-            (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
+            (make-variable-buffer-local 'ac-sources)
 
-;; php-completion
-(add-hook 'php-mode-hook
-          '(lambda ()
-             (require 'php-completion)
-             (php-completion-mode t)
-             (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)))
+            ;; If you like patial match, use `ac-source-php-completion-patial'
+            ;; instead of `ac-source-php-completion':
+            (add-to-list 'ac-sources 'ac-source-php-completion)
+            ;; (add-to-list 'ac-sources 'ac-source-php-completion-patial)
 
-(add-hook 'php-mode-hook
-          '(lambda () (define-abbrev php-mode-abbrev-table "ex" "extends")))
+            (auto-complete-mode t))))
 
-;; Cooperation with auto-complete.el:
-(add-hook 'php-mode-hook
-          '(lambda ()
-             (when (require 'auto-complete nil t)
-               (make-variable-buffer-local 'ac-sources)
-               (add-to-list 'ac-sources 'ac-source-php-completion)
-               ;; if you like patial match,
-               ;; use `ac-source-php-completion-patial' instead of `ac-source-php-completion'.
-               ;(add-to-list 'ac-sources 'ac-source-php-completion-patial)
-               (auto-complete-mode t))))
 
-;; Custom coding standards
-(add-hook 'php-mode-hook
-          (lambda ()
-            (setq indent-tabs-mode nil)
-            (setq tab-width (default-value 'tab-width))
-            ))
-
-;; Automatic ya-snippets for Php
-(require 'php-auto-yasnippets)
-
-;; (define-key php-mode-map (kbd "C-c C-y") 'yas/create-php-snippet)
-
-;; (payas/ac-setup) ; auto-complete setup
+(use-package php-auto-yasnippets
+  :bind ("C-c C-y" . yas/create-php-snippet)
+  :config
+  ;; Auto-complete setup
+  ;; (payas/ac-setup)
+  )
