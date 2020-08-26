5089e3812d542f195fca7a58453071501967d15b
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-26 17:23:08 +0300

    Use-package on flycheck.el

--- rc/flycheck.el
+++ rc/flycheck.el
@@ -4,20 +4,27 @@
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+
 ;; Clojure -- flycheck-clojure
 (eval-after-load 'flycheck '(flycheck-clojure-setup))
+
 
 ;; Haskell -- flycheck-haskell
 (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)
 
+
 ;; Python -- flycheck-pyflakes
-(require 'flycheck-pyflakes)
-(add-hook 'python-mode-hook 'flycheck-mode)
+(use-package flycheck-pyflakes
+  :hook (python-mode . flycheck-mode))
 
 (add-to-list 'flycheck-disabled-checkers 'python-flake8)
 (add-to-list 'flycheck-disabled-checkers 'python-pylint)
 
-;; Fix PATH variable ; TODO Move elsewhere
-;; http://www.flycheck.org/en/latest/
-(require 'exec-path-from-shell)
-(exec-path-from-shell-initialize)
+
+;; For a GUI Emacs on MacOS we recommend to install and configure
+;; exec-path-from-shell to make Emacs use the proper $PATH and avoid a common
+;; setup issue on MacOS. See: http://www.flycheck.org/en/latest/
+(when (and (eq system-type "darwin") window-system)
+  (use-package exec-path-from-shell
+    :config
+    (exec-path-from-shell-initialize)))
