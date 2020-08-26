b7cb0b144ebe478395a1649a59426740b435a47f
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-26 20:32:56 +0300

    Use-package on auto-complete.el

--- rc/auto-completion.el
+++ rc/auto-completion.el
@@ -2,50 +2,51 @@
 ;; Auto completion
 ;; ------------------------------------------------------------------------------
 
-(require 'auto-complete)
-(auto-complete-mode t)
-(global-set-key "\M-q" 'ac-start)
+(use-package auto-complete
+  :bind ("M-q" . ac-start)
+  :config (auto-complete-mode t))
 
-(require 'ac-dabbrev)
-(global-set-key '[C-M-tab] 'dabbrev-expand)
 
-;;; Tips:
-;;
+(use-package ac-dabbrev
+  :bind ("<C-M-tab>" . dabbrev-expand))
+
+
 ;; Use C-n/C-p to select candidates
-;; --------------------------------
 (define-key ac-complete-mode-map "\C-n" 'ac-next)
 (define-key ac-complete-mode-map "\C-p" 'ac-previous)
 
-;; Don't start completion automatically
-;; ------------------------------------
-;;
-;; Add following code to your .emacs.
-;;
-;;     (setq ac-auto-start nil)
-;; or
-;;     ;; start completion when entered 3 characters
-;;     (setq ac-auto-start 3)
-
-;;
 ;; Stop completion
-;; ---------------
-;;
-;; Add following code to your .emacs.
-;;
-(define-key ac-complete-mode-map "\M-q" 'ac-stop)
+(define-key ac-complete-mode-map "\M-." 'ac-stop)
 
-;;
-;; Now you can stop completion by pressing M-/.
-;;
-;;
+;; Autostart completion
+;; (setq ac-auto-start nil)
+;; Or start completion when entered 3 characters
+;; (setq ac-auto-start 3)
+
+
+;; Change default sources
+(setq ac-sources
+      '(ac-source-abbrev
+        ac-source-dabbrev
+        ;; ac-source-gtags
+        ;; ac-source-yasnippet
+        ac-source-words-in-buffer))
+
+;; Change sources for particular mode
+;; (add-hook 'emacs-lisp-mode-hook
+;;   (lambda ()
+;;     (setq ac-sources '(ac-source-words-in-buffer ac-source-symbols))))
+
+
+;; =============================================================================
+;; Auto complete tips
+;; -----------------------------------------------------------------------------
+
 ;; Completion by TAB
 ;; -----------------
 ;;
-;; Add following code to your .emacs.
-;;
-;;     (define-key ac-complete-mode-map "\t" 'ac-complete)
-;;     (define-key ac-complete-mode-map "\r" nil)
-;;
+;; (define-key ac-complete-mode-map "\t" 'ac-complete)
+;; (define-key ac-complete-mode-map "\r" nil)
 ;;
 ;; Do What I Mean mode
 ;; -------------------
@@ -63,23 +64,5 @@
 ;; DWIM mode is enabled by default.
 ;; You can enable this feature by
 ;; setting `ac-dwim' to t.
-;;
-(setq ac-dwim t)
-;;
-;;
-;; Change default sources
-;; ----------------------
-;;
-(setq ac-sources
-      '(ac-source-abbrev
-        ac-source-dabbrev
-        ;; ac-source-gtags
-        ;; ac-source-yasnippet
-        ac-source-words-in-buffer))
 
-;; Change sources for particular mode
-;; ----------------------------------
-;;
-;;     (add-hook 'emacs-lisp-mode-hook
-;;                 (lambda ()
-;;                   (setq ac-sources '(ac-source-words-in-buffer ac-source-symbols))))
+(setq ac-dwim t)
