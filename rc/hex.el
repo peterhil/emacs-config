7a75ea206bf7c668063672598df347e11dcecfa0
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-26 17:15:41 +0300

    Use-package on hex.el

--- rc/hex.el
+++ rc/hex.el
@@ -2,5 +2,13 @@
 ;; Hex and binary file format editing
 ;; ------------------------------------------------------------------------------
 
-(require 'hexview-mode)
-(require 'binview)
+;; Better alternative to the builtin hexl-mode
+(use-package nhexl-mode)
+
+
+(use-package hexview-mode
+  :load-path "site-lisp/hexview-mode")
+
+
+(use-package binview
+  :load-path "site-lisp/binview")
