b968833b2d31de55d7961250d366e20ac8d35ec6
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-26 16:51:04 +0300

    Use-package on multi-web-mode.el

--- rc/multi-web-mode.el
+++ rc/multi-web-mode.el
@@ -2,12 +2,13 @@
 ;; Multi web mode
 ;; ------------------------------------------------------------------------------
 
-(require 'multi-web-mode)
-
-(setq mweb-default-major-mode 'html-mode)
-(setq mweb-submode-indent-offset 4)
-(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-                  (js2-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("php[3-5]?" "twig" "html?" "ctp"))
-;(multi-web-global-mode t)
+(use-package multi-web-mode
+  :config
+  (setq mweb-default-major-mode 'html-mode)
+  (setq mweb-submode-indent-offset 4)
+  (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                     (js2-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                     (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+  (setq mweb-filename-extensions '("php[3-5]?" "twig" "html?" "ctp"))
+  ;; (multi-web-global-mode t)
+  )
