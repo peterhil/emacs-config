dcf61236b0c9bad8911df1235b4e001b7b98268e
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-26 04:35:46 +0300

    Use-package on markdown.el

--- rc/markdown.el
+++ rc/markdown.el
@@ -2,11 +2,8 @@
 ;; Markdown
 ;; ------------------------------------------------------------------------------
 
-(require 'markdown-mode)
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
-
-(my-auto-mode-extensions
- 'markdown-mode
- '("md"
-   "markdown"))
+(use-package "markdown-mode"
+  :mode
+  (
+    "\\.md\\'"
+    ))
