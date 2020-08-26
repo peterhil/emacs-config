86f628b68f5cee7a5ca6bc5326f7351137230fcc
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-26 03:47:41 +0300

    Use-package on js.el

--- rc/js.el
+++ rc/js.el
@@ -3,18 +3,19 @@
 ;; ------------------------------------------------------------------------------
 
 ;; (require 'javascript-mode)
-(autoload 'javascript-mode "javascript-mode" nil t)
+;; (autoload 'javascript-mode "javascript-mode" nil t)
 
-;; https://stackoverflow.com/questions/35162106/how-to-disable-js2-mode-syntax-checking-globally-in-spacemacs
-(setq js2-strict-missing-semi-warning nil)
+(defun my-disable-electric-indent ()
+  (set (make-local-variable 'electric-indent-chars) nil))
 
-(my-auto-mode-extensions
- 'javascript-mode
- '(
-   "js"
-   "ts"
-   ))
-
-(add-hook 'javascript-mode-hook
-          '(lambda()
-             (set (make-local-variable 'electric-indent-chars) nil)))
+(use-package "js2-mode"
+  :mode
+  (
+    "\\.js\\'"
+    "\\.ts\\'"
+    )
+  ;; :hook (js2-mode . 'my-disable-electric-indent)
+  :config
+  ;; https://stackoverflow.com/questions/35162106/how-to-disable-js2-mode-syntax-checking-globally-in-spacemacs
+  (setq js2-strict-missing-semi-warning nil)
+  )
