856f1a4c07648034148b93df705d097ca500fa79
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-08-26 06:47:51 +0300

    Use-package on regexp.el

--- rc/regexp.el
+++ rc/regexp.el
@@ -3,8 +3,10 @@
 ;; https://github.com/benma/visual-regexp.el
 ;; ==============================================================================
 
-(require 'visual-regexp)
-(define-key global-map (kbd "C-c r") 'vr/replace)
-(define-key global-map (kbd "C-c q") 'vr/query-replace)
-;; if you use multiple-cursors, this is for you:
-(define-key global-map (kbd "C-c m") 'vr/mc-mark)
+(use-package "visual-regexp"
+  :bind
+  (
+    ("C-c r" . vr/replace)
+    ("C-c q" . vr/query-replace)
+    ;; if you use multiple-cursors, this is for you:
+    ("C-c m" . vr/mc-mark)))
