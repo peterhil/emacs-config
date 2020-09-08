5a9512d1df46325a0734bc5817c759c632ece883
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-09-08 20:03:50 +0300

    Fix fortune on Mac OS X desktop by using exec-path-from-shell

--- rc/shell.el
+++ rc/shell.el
@@ -2,18 +2,21 @@
 ;; Shell
 ;; ------------------------------------------------------------------------------
 
+;; Ensure that environment variables inside Emacs look the same as in the user's shell
 ;; https://github.com/purcell/exec-path-from-shell
 (use-package exec-path-from-shell
   :init
   (when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize)))
+    (exec-path-from-shell-initialize)))
 
-;; (add-hook 'shell-mode-hook 'dark-shell)
 
 ;; Open dark-themed shell
-(defun dark-shell () 
+(defun my-dark-shell ()
   (interactive)
   (let ((color-theme-is-global nil))
     (select-frame (make-frame))
     (color-theme-jsc-dark)
     (shell)))
+
+
+;; (add-hook 'shell-mode-hook 'my-dark-shell)
