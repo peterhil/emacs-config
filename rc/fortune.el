a0ae1de614a5930c01c6d669d8472e71bbb92949
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-09-08 18:44:21 +0300

    Fix fortune-cookie config on Mac and Linux

--- rc/fortune.el
+++ rc/fortune.el
@@ -3,11 +3,20 @@
 ;; https://github.com/andschwa/fortune-cookie
 ;; -----------------------------------------------------------------------------
 
-;; inhibit startup message
+;; Inhibit startup message
 (setq inhibit-startup-message t)
 
+
+;; Show a fortune on *scratch* buffer on startup
 (use-package "fortune-cookie"
-  :commands fortune
   :init
   ;; (setq fortune-cookie-cowsay-args  "-f blowfish")
   (fortune-cookie-mode))
+
+
+(defun my-get-fortune ()
+  (interactive)
+  (setq fortune-dir "/usr/local/share/games/fortunes")
+  (setq fortune-file (expand-file-name "cookie" fortune-dir))
+  (fortune)
+  (get-buffer "*fortune*"))
