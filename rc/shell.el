c77c0d115b0e9f31ec250c8ae70c7bfcff5f85e5
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-09-12 18:00:04 +0300

    Disable exec-path-from-shell on X window system

--- rc/shell.el
+++ rc/shell.el
@@ -5,7 +5,7 @@
 ;; Ensure that environment variables inside Emacs look the same as in the user's shell
 ;; https://github.com/purcell/exec-path-from-shell
 (use-package exec-path-from-shell
-  :if (memq window-system '(mac ns x))
+  :if (memq window-system '(mac ns))
   :init
   (exec-path-from-shell-initialize))
 
