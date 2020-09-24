17f4a16ec1732cfc0ac2a4defdca2a546c0c57f6
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-09-24 15:30:59 +0300

    Leave dired-use-ls-dired unspecified so it checks whether --dired option is supported by ls

--- rc/custom.el
+++ rc/custom.el
@@ -25,7 +25,6 @@
   '(default-input-method "finnish-keyboard")
   '(desktop-save-mode nil)
   '(dired-auto-revert-buffer 'dired-directory-changed-p)
-  '(dired-use-ls-dired nil)
   '(emulate-mac-finnish-keyboard-mode t)
   '(fci-rule-color "#4E5959")
   '(font-use-system-font t)
