1b6d1e5c91d07064b8a2b6984e11bf323c62be40
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-10 03:28:47 +0300

    Fix use-package forms on style.el

--- rc/style.el
+++ rc/style.el
@@ -3,37 +3,54 @@
 ;; ------------------------------------------------------------------------------
 
 ;; Jinja2
-(use-package "jinja2-mode"
+(use-package jinja2-mode
   :mode
   ("\\.jinja\\'"
    ;; Do matching on the path and extension
    "/templates/.*\\.html\\'"))
+
 
 ;; ==============================================================================
 ;; CSS modes
 ;; ------------------------------------------------------------------------------
 
 ;; Css
-(use-package "css-mode" :mode "\\.css\\'")
+(use-package css-mode :mode "\\.css\\'")
 
 ;; Jade
-(use-package "jade-mode" :mode "\\.jade\\'")
+(use-package jade-mode :mode "\\.jade\\'")
 
 ;; Less CSS
-(use-package "less-css-mode" :mode "\\.less\\'")
+(use-package less-css-mode :mode "\\.less\\'")
 
-;; Sass and Scss
-(use-package "scss-mode"
+;; Sass
+(use-package sass-mode
+  :commands (sass-mode)
+  :mode "\\.sass\\'"
+  :init
+  ;; (add-to-list 'auto-mode-alist '("\\.sass\\'" . sass-mode))
+  )
+
+;; Scss
+(use-package scss-mode
+  :ensure t
+  ;; :defer t
+  :commands (scss-mode scss-compile css-mode web-mode)
+  :mode ("\\.scss" . scss-mode)
+  ;; :init
+  ;; (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
   :config
-  (setq scss-compile-at-save nil)
-  :mode
-  (
-    ("\\.sass\\'" . sass-mode)
-    ("\\.scss\\'" . scss-mode)
-    ))
+  (require 'scss-mode)
+  (setq scss-sass-command
+        'dart-p
+        ;; (expand-file-name "~/.nix-profile/bin/sass")
+        ;; "sass"
+        )
+  (setq scss-compile-at-save nil) ;; Compile buffer at save
+  )
 
 ;; Stylus
-(use-package "stylus-mode" :mode "\\.styl\\'")
+(use-package stylus-mode :mode "\\.styl\\'")
 
 ;; Yaml
-(use-package "yaml-mode" :mode "\\.ya?ml\\'")
+(use-package yaml-mode :mode "\\.ya?ml\\'")
