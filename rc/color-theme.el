2748ee71d2d14b0ef20f96f0428ac72289e81b3f
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-10 22:53:39 +0300

    Add current customisable monokai theme colours into config with their default values

--- rc/color-theme.el
+++ rc/color-theme.el
@@ -18,4 +18,26 @@
 (use-package monokai-theme
   :ensure t
   :init
-  (load-theme 'monokai t))
+  (load-theme 'monokai t)
+
+  (setq
+   ;; foreground and background
+   monokai-foreground     "#F8F8F2"
+   monokai-background     "#272822"
+   ;; highlights and comments
+   monokai-comments       "#75715E"
+   monokai-emphasis       "#F8F8F0"
+   monokai-highlight      "#49483E"
+   monokai-highlight-alt  "#3E3D31"
+   monokai-highlight-line "#3C3D37"
+   monokai-line-number    "#8F908A"
+   ;; colours
+   monokai-blue           "#66D9EF"
+   monokai-cyan           "#A1EFE4"
+   monokai-green          "#A6E22E"
+   monokai-gray           "#64645E"
+   monokai-violet         "#AE81FF"
+   monokai-red            "#F92672"
+   monokai-orange         "#FD971F"
+   monokai-yellow         "#E6DB74"
+   ))
