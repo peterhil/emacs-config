a2de9dee9181a67fff3ad4032a40fe57bdb7eece
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-11 00:45:34 +0300

    Fix setting custom monokai theme colours and add org-mode heading sizes

--- rc/color-theme.el
+++ rc/color-theme.el
@@ -15,29 +15,35 @@
 ;; (load-theme 'spacemacs-dark t)
 ;; (load-theme 'zerodark t)
 
+(setq
+ ;; foreground and background
+ monokai-foreground     "#F8F8F2"
+ monokai-background     "#222627"
+ ;; highlights and comments
+ monokai-comments       "#71756E"
+ monokai-emphasis       "#F8F8F0"
+ monokai-highlight      "#364142"
+ monokai-highlight-alt  "#313D3E"
+ monokai-highlight-line "#4E5959"
+ monokai-line-number    "#71756E"
+ ;; colours
+ monokai-blue           "#66D9EF"
+ monokai-cyan           "#A1EFE4"
+ monokai-green          "#A6E22E"
+ monokai-gray           "#64645E"
+ monokai-violet         "#AE81FF"
+ monokai-red            "#F92672"
+ monokai-orange         "#FD971F"
+ monokai-yellow         "#E6DB74")
+
+(setq
+ monokai-height-minus-1 0.8
+ monokai-height-plus-1 1.1
+ monokai-height-plus-2 1.15
+ monokai-height-plus-3 1.2
+ monokai-height-plus-4 1.3)
+
 (use-package monokai-theme
   :ensure t
   :init
-  (load-theme 'monokai t)
-
-  (setq
-   ;; foreground and background
-   monokai-foreground     "#F8F8F2"
-   monokai-background     "#222627"
-   ;; highlights and comments
-   monokai-comments       "#71756E"
-   monokai-emphasis       "#F8F8F0"
-   monokai-highlight      "#364142"
-   monokai-highlight-alt  "#313D3E"
-   monokai-highlight-line "#4E5959"
-   monokai-line-number    "#71756E"
-   ;; colours
-   monokai-blue           "#66D9EF"
-   monokai-cyan           "#A1EFE4"
-   monokai-green          "#A6E22E"
-   monokai-gray           "#64645E"
-   monokai-violet         "#AE81FF"
-   monokai-red            "#F92672"
-   monokai-orange         "#FD971F"
-   monokai-yellow         "#E6DB74"
-   ))
+  (load-theme 'monokai t))
