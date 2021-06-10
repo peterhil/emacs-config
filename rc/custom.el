50f34557d1f6d3613b87df39219ba93a161c0d6d
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-10 22:10:31 +0300

    Fix indentation on custom.el

--- rc/custom.el
+++ rc/custom.el
@@ -3,95 +3,95 @@
 ;; ------------------------------------------------------------------------------
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(ansi-color-names-vector
-     ["#222627" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
-  '(anything-command-map-prefix-key "C-x a")
-  '(better-fringes-mode t)
-  '(c-echo-syntactic-information-p t)
-  '(c-special-indent-hook '(ignore))
-  '(c-tab-always-indent 'other)
-  '(color-theme-is-cumulative nil)
-  '(column-number-mode t)
-  '(compilation-message-face 'default)
-  '(create-frame-min-width 120)
-  '(current-language-environment "UTF-8")
-  '(custom-safe-themes
-     '("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default))
-  '(debug-on-error t)
-  '(default-input-method "finnish-keyboard")
-  '(desktop-save-mode nil)
-  '(dired-auto-revert-buffer 'dired-directory-changed-p)
-  '(emulate-mac-finnish-keyboard-mode t)
-  '(fci-rule-color "#4E5959")
-  '(font-use-system-font t)
-  '(highlight-changes-colors ("#FD5FF0" "#AE81FF"))
-  '(highlight-tail-colors
-     '(("#4E5959" . 0)
-        ("#67930F" . 20)
-        ("#349B8D" . 30)
-        ("#21889B" . 50)
-        ("#968B26" . 60)
-        ("#A45E0A" . 70)
-        ("#A41F99" . 85)
-        ("#4E5959" . 100)))
-  '(indent-tabs-mode nil)
-  '(indicate-buffer-boundaries 'left)
-  '(inhibit-startup-screen t)
-  '(ns-tool-bar-display-mode 'labels t)
-  '(ns-tool-bar-size-mode 'regular t)
-  '(one-buffer-one-frame-mode nil)
-  '(package-selected-packages
-     '(2048-game ztree lsp-julia lsp-mode zig-mode svelte-mode elmacro nhexl-mode quickrun reason-mode rust-mode expand-region markdown-mode nxml-mode elpy edit-server csv-mode undo-tree hen scheme-complete zeal-at-point use-package-ensure-system-package utop merlin fill-column-indicator faustine faust-mode fortune-cookie form-feed magit-delta fireplace use-package d-mode auto-minor-mode picolisp-mode lua-mode flymake-jslint visual-regexp auto-complete caml tuareg feature-mode vue-html-mode vue-mode typescript-mode xref-js2 zencoding-mode eslint-fix exec-path-from-shell elm-mode csv-nav php-auto-yasnippets zonokai-theme yaml-mode web-mode w3m vmd-mode uuidgen stylus-mode spacegray-theme smartparens slime scss-mode sass-mode ruby-compilation rainbow-mode qml-mode python-mode projectile php-mode php-completion paredit move-text monokai-theme mkdown md-readme markdown-mode+ linum-off less-css-mode json-mode jinja2-mode jazz-theme jade-mode hlinum grizzl fuzzy-format flymd flymake-php flymake-json flymake-css flymake-coffee flycheck-pyflakes flycheck-haskell flycheck-clojure erlang elixir-mix editorconfig cypher-mode color-theme-sanityinc-tomorrow coffee-mode anything alchemist ag ack-and-a-half ace-jump-mode ac-dabbrev))
-  '(safe-local-variable-values
-     '((encoding . utf-8)
-        (Syntax . ANSI-Common-Lisp)
-        (syntax . ANSI-Common-Lisp)))
-  '(save-place-mode t nil (saveplace))
-  '(select-enable-clipboard t)
-  '(semantic-stickyfunc-indent-string " ")
-  '(show-paren-mode t)
-  '(show-trailing-whitespace t)
-  '(size-indication-mode t)
-  '(split-width-threshold 240)
-  '(tab-always-indent t)
-  '(tab-width 4)
-  '(text-mode-hook '(turn-on-auto-fill text-mode-hook-identify))
-  '(tool-bar-mode nil)
-  '(uniquify-buffer-name-style 'forward nil (uniquify))
-  '(vc-annotate-background nil)
-  '(vc-annotate-color-map
-     '((20 . "#F92672")
-        (40 . "#CF4F1F")
-        (60 . "#C26C0F")
-        (80 . "#E6DB74")
-        (100 . "#AB8C00")
-        (120 . "#A18F00")
-        (140 . "#989200")
-        (160 . "#8E9500")
-        (180 . "#A6E22E")
-        (200 . "#729A1E")
-        (220 . "#609C3C")
-        (240 . "#4E9D5B")
-        (260 . "#3C9F79")
-        (280 . "#A1EFE4")
-        (300 . "#299BA6")
-        (320 . "#2896B5")
-        (340 . "#2790C3")
-        (360 . "#66D9EF")))
-  '(vc-annotate-very-old-color nil)
-  '(visual-line-mode nil t)
-  '(weechat-color-list
-     (unspecified "#222627" "#4E5959" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))
-  '(yaml-indent-offset 4))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#222627" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(anything-command-map-prefix-key "C-x a")
+ '(better-fringes-mode t)
+ '(c-echo-syntactic-information-p t)
+ '(c-special-indent-hook '(ignore))
+ '(c-tab-always-indent 'other)
+ '(color-theme-is-cumulative nil)
+ '(column-number-mode t)
+ '(compilation-message-face 'default)
+ '(create-frame-min-width 120)
+ '(current-language-environment "UTF-8")
+ '(custom-safe-themes
+   '("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default))
+ '(debug-on-error t)
+ '(default-input-method "finnish-keyboard")
+ '(desktop-save-mode nil)
+ '(dired-auto-revert-buffer 'dired-directory-changed-p)
+ '(emulate-mac-finnish-keyboard-mode t)
+ '(fci-rule-color "#4E5959")
+ '(font-use-system-font t)
+ '(highlight-changes-colors ("#FD5FF0" "#AE81FF"))
+ '(highlight-tail-colors
+   '(("#4E5959" . 0)
+     ("#67930F" . 20)
+     ("#349B8D" . 30)
+     ("#21889B" . 50)
+     ("#968B26" . 60)
+     ("#A45E0A" . 70)
+     ("#A41F99" . 85)
+     ("#4E5959" . 100)))
+ '(indent-tabs-mode nil)
+ '(indicate-buffer-boundaries 'left)
+ '(inhibit-startup-screen t)
+ '(ns-tool-bar-display-mode 'labels t)
+ '(ns-tool-bar-size-mode 'regular t)
+ '(one-buffer-one-frame-mode nil)
+ '(package-selected-packages
+   '(2048-game ztree lsp-julia lsp-mode zig-mode svelte-mode elmacro nhexl-mode quickrun reason-mode rust-mode expand-region markdown-mode nxml-mode elpy edit-server csv-mode undo-tree hen scheme-complete zeal-at-point use-package-ensure-system-package utop merlin fill-column-indicator faustine faust-mode fortune-cookie form-feed magit-delta fireplace use-package d-mode auto-minor-mode picolisp-mode lua-mode flymake-jslint visual-regexp auto-complete caml tuareg feature-mode vue-html-mode vue-mode typescript-mode xref-js2 zencoding-mode eslint-fix exec-path-from-shell elm-mode csv-nav php-auto-yasnippets zonokai-theme yaml-mode web-mode w3m vmd-mode uuidgen stylus-mode spacegray-theme smartparens slime scss-mode sass-mode ruby-compilation rainbow-mode qml-mode python-mode projectile php-mode php-completion paredit move-text monokai-theme mkdown md-readme markdown-mode+ linum-off less-css-mode json-mode jinja2-mode jazz-theme jade-mode hlinum grizzl fuzzy-format flymd flymake-php flymake-json flymake-css flymake-coffee flycheck-pyflakes flycheck-haskell flycheck-clojure erlang elixir-mix editorconfig cypher-mode color-theme-sanityinc-tomorrow coffee-mode anything alchemist ag ack-and-a-half ace-jump-mode ac-dabbrev))
+ '(safe-local-variable-values
+   '((encoding . utf-8)
+     (Syntax . ANSI-Common-Lisp)
+     (syntax . ANSI-Common-Lisp)))
+ '(save-place-mode t nil (saveplace))
+ '(select-enable-clipboard t)
+ '(semantic-stickyfunc-indent-string " ")
+ '(show-paren-mode t)
+ '(show-trailing-whitespace t)
+ '(size-indication-mode t)
+ '(split-width-threshold 240)
+ '(tab-always-indent t)
+ '(tab-width 4)
+ '(text-mode-hook '(turn-on-auto-fill text-mode-hook-identify))
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style 'forward nil (uniquify))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF")))
+ '(vc-annotate-very-old-color nil)
+ '(visual-line-mode nil t)
+ '(weechat-color-list
+   (unspecified "#222627" "#4E5959" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))
+ '(yaml-indent-offset 4))
 
 
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
