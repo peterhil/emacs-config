b252c30507d9c0e8efe1cd55d2a5c84750c9c247
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-10 22:29:07 +0300

    use-package: Monokai theme from Melpa

--- rc/color-theme.el
+++ rc/color-theme.el
@@ -2,23 +2,20 @@
 ;; Color Theme
 ;; -----------------------------------------------------------------------------
 
-;; Themes form Melpa
-;; -----------------------------------------------------------------------------
 ;; (if (> (display-color-cells) 256)
 ;;     (load-theme 'monokai t)
 ;;   (load-theme 'jazz t))
-
-(progn
-  (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/monokai-emacs"))
-  (load-theme 'monokai t))
 
 ;; (progn
 ;;   (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/color-theme-mgl"))
 ;;   (load-theme 'mgl-dark t))
 
 ;; (load-theme 'spacegray t)
-
 ;; (load-theme 'spacemacs-light t)
 ;; (load-theme 'spacemacs-dark t)
-
 ;; (load-theme 'zerodark t)
+
+(use-package monokai-theme
+  :ensure t
+  :init
+  (load-theme 'monokai t))
