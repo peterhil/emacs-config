4059281304d41865c5c381a799a16702004c6636
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-11-22 06:40:01 +0200

    Add lsp related config and packages

--- rc/lsp.el
+++ rc/lsp.el
@@ -16,7 +16,27 @@
   (setq lsp-keymap-prefix "C-c l")
 
   ;; https://github.com/zigtools/zls/
-  (setq lsp-zig-zls-executable "/Users/peterhil/code/src/zig/zls/zls-git/zig-out/bin/zls"))
+  (setq lsp-zig-zls-executable "zls"))
 
 
 (use-package lsp-ui :commands lsp-ui-mode)
+
+
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+;; (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+
+
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+
+;; optionally if you want to use debugger
+;; (use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+
+;; optional if you want which-key integration
+(use-package which-key
+  :config
+  (which-key-mode))
