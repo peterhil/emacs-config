e0ce1b39a2f8a32be304d568e23eef88b30761ee
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-11-25 04:21:10 +0200

    Remove setting of lsp-zig-zls-executable

--- rc/lsp.el
+++ rc/lsp.el
@@ -14,10 +14,7 @@
          (zig-mode-hook . lsp-deferred))
   :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-
-  ;; https://github.com/zigtools/zls/
-  (setq lsp-zig-zls-executable "zls"))
+  (setq lsp-keymap-prefix "C-c l"))
 
 
 (use-package lsp-ui :commands lsp-ui-mode)
