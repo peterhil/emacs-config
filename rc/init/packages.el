cce98a85c8e3975c6dffa7cda99016e5ef3088e5
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-05 10:36:18 +0200

    Reindent my-packages list

--- rc/init/packages.el
+++ rc/init/packages.el
@@ -2,7 +2,6 @@
 ;; My packages
 ;; ------------------------------------------------------------------------------
 
-
 (defun package-update-load-path ()
   "Update the load path for newly installed packages."
   (interactive)
@@ -22,113 +21,112 @@
               (add-to-list 'load-path path)))
           package-alist)))
 
-
-(defvar my-packages '(
-                      ;; ac-dabbrev
-                      ;; ac-python
-                      ace-jump-mode
-                      ;; ack
-                      ;; ack-and-a-half
-                      ag
-                      ;; alchemist
-                      ;; anything
-                      auto-complete
-                      auto-minor-mode
-                      ;; caml
-                      clojure-mode
-                      ;; coffee-mode
-                      csv-mode
-                      csv-nav
-                      ;; cypher-mode
-                      ;; d-mode
-                      editorconfig
-                      ;; elixir-mix
-                      ;; elixir-mode
-                      ;; elm-mode
-                      ;; erlang
-                      f
-                      ;; faust-mode
-                      ;; faustine
-                      ;; feature-mode
-                      fill-column-indicator
-                      ;; fireplace
-                      flycheck
-                      flycheck-clojure
-                      ;; flycheck-haskell
-                      flycheck-pyflakes
-                      flymake
-                      ;; flymake-coffee
-                      flymake-css
-                      flymake-json
-                      ;; flymake-php
-                      ;; flymd
-                      fortune-cookie
-                      ;; fuzzy-format
-                      ;; git-commit-mode
-                      ;; git-rebase-mode
-                      ;; grizzl
-                      ;; haml-mode
-                      hlinum
-                      ;; jade-mode
-                      jinja2-mode
-                      js2-mode
-                      json-mode
-                      lua-mode
-                      less-css-mode
-                      let-alist
-                      linum-off
-                      magit
-                      markdown-mode
-                      md-readme
-                      ;; merlin
-                      mkdown
-                      move-text
-                      org
-                      ;; ox-gfm
-                      paredit
-                      paredit-everywhere
-                      paredit-menu
-                      parinfer
-                      paxedit
-                      php-auto-yasnippets
-                      php-completion
-                      php-mode
-                      ;; projectile
-                      python-mode
-                      ;; qml-mode
-                      rainbow-mode
-                      ;; ruby-compilation
-                      s
-                      sass-mode
-                      scss-mode
-                      slime
-                      smartparens
-                      stylus-mode
-                      ;; tagedit
-                      ;; tuareg
-                      use-package
-                      utop
-                      ;; uuidgen
-                      visual-regexp
-                      ;; vmd-mode
-                      w3m
-                      web-mode
-                      ;; web-server
-                      ;; websocket
-                      yaml-mode
-                      zig-mode
-                      ;; THEMES
-                      dracula-theme
-                      mbo70s-theme
-                      monokai-theme
-                      solarized-theme
-                      spacegray-theme
-                      spacemacs-theme
-                      sublime-themes
-                      zonokai-theme
-                      )
+(defvar my-packages
+  '(
+    ;; ac-dabbrev
+    ;; ac-python
+    ace-jump-mode
+    ;; ack
+    ;; ack-and-a-half
+    ag
+    ;; alchemist
+    ;; anything
+    auto-complete
+    auto-minor-mode
+    ;; caml
+    clojure-mode
+    ;; coffee-mode
+    csv-mode
+    csv-nav
+    ;; cypher-mode
+    ;; d-mode
+    editorconfig
+    ;; elixir-mix
+    ;; elixir-mode
+    ;; elm-mode
+    ;; erlang
+    f
+    ;; faust-mode
+    ;; faustine
+    ;; feature-mode
+    fill-column-indicator
+    ;; fireplace
+    flycheck
+    flycheck-clojure
+    ;; flycheck-haskell
+    flycheck-pyflakes
+    flymake
+    ;; flymake-coffee
+    flymake-css
+    flymake-json
+    ;; flymake-php
+    ;; flymd
+    fortune-cookie
+    ;; fuzzy-format
+    ;; git-commit-mode
+    ;; git-rebase-mode
+    ;; grizzl
+    ;; haml-mode
+    hlinum
+    ;; jade-mode
+    jinja2-mode
+    js2-mode
+    json-mode
+    lua-mode
+    less-css-mode
+    let-alist
+    linum-off
+    magit
+    markdown-mode
+    md-readme
+    ;; merlin
+    mkdown
+    move-text
+    org
+    ;; ox-gfm
+    paredit
+    paredit-everywhere
+    paredit-menu
+    parinfer
+    paxedit
+    php-auto-yasnippets
+    php-completion
+    php-mode
+    ;; projectile
+    python-mode
+    ;; qml-mode
+    rainbow-mode
+    ;; ruby-compilation
+    s
+    sass-mode
+    scss-mode
+    slime
+    smartparens
+    stylus-mode
+    ;; tagedit
+    ;; tuareg
+    use-package
+    utop
+    ;; uuidgen
+    visual-regexp
+    ;; vmd-mode
+    w3m
+    web-mode
+    ;; web-server
+    ;; websocket
+    yaml-mode
+    zig-mode
+    ;; THEMES
+    dracula-theme
+    mbo70s-theme
+    monokai-theme
+    solarized-theme
+    spacegray-theme
+    spacemacs-theme
+    sublime-themes
+    zonokai-theme
+    )
   "A list of packages to ensure are installed at launch.")
-
 
 (defun my-install-package-list ()
   (dolist (p my-packages)
