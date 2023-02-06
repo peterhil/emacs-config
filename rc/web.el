3d178697b0b2e3bf7f6ef6d1703e269736e6595c
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-06 02:57:19 +0200

    Rewrite web-mode modes with rx

--- rc/web.el
+++ rc/web.el
@@ -8,29 +8,44 @@
     (setq web-mode-code-indent-offset 4)
     (setq web-mode-css-indent-offset 4)
     (setq web-mode-markup-indent-offset 2))
-  :magic
-  (
-    "<\\!DOCTYPE"
-    "<\\!doctype"
-    "<html"
-    )
+  :magic ("<\\!DOCTYPE"
+          "<\\!doctype"
+          "<html")
   :mode
-  (
-    "\\.ctp\\'"
-    "\\.erb\\'"
-    "\\.html?\\'"
-    "\\.jsx\\'"
-    "\\.marko\\'"
-    "\\.mustache\\'"
-    "\\.php\\'"
-    ;; "\\.svelte\\'"
-    "\\.tag\\'"
-    "\\.vue\\'"
-    "\\.xhtml\\'"
-    ;; Matching can be done on the path
-    "/\\(views\\|html\\|templates\\)/.*\\.\\(php\\|twig\\)\\'"
-    "/components/.*\\.js\\'"
-    ))
+  ((rx "."
+       (or
+        "ctp"
+        "erb"
+        "html?"
+        "jsx"
+        "marko"
+        "mustache"
+        "php"
+        ;; "svelte"
+        "tag"
+        "vue"
+        "xhtml"
+        )
+       line-end)
+   ;; Matching can be done on the path
+   (rx (seq "/"
+            (or
+             "html"
+             "templates"
+             "views"
+             )
+            "/")
+       (* (seq (* alnum) "/"))
+       ;; Filename
+       (+ alnum)
+       "."
+       (or "php" "twig")
+       line-end)
+   (rx "/components/"
+       (* (seq (* alnum) "/"))
+       (+ alnum)
+       ".js"
+       line-end)))
 
 (use-package web-mode-edit-element
   :hook (web-mode-hook 'web-mode-edit-element-minor-mode))
