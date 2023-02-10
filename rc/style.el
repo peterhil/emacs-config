1fb684ed1f29eba548343ab0538380342f89a2a6
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-10 09:31:31 +0200

    Fix style.el autoloads

--- rc/style.el
+++ rc/style.el
@@ -5,7 +5,7 @@
 ;; Jinja2
 (use-package jinja2-mode
   :mode
-  ("\\.jinja\\'"
+  (".jinja"
    ;; Do matching on the path and extension
    "/templates/.*\\.html\\'"))
 
@@ -15,38 +15,37 @@
 ;; ------------------------------------------------------------------------------
 
 ;; Css
-(use-package css-mode :mode "\\.css\\'")
+(use-package css-mode
+  :mode (".css" . css-mode))
 
 ;; Jade
-(use-package jade-mode :mode "\\.jade\\'")
+(use-package jade-mode
+  :mode (".jade" . jade-mode))
 
 ;; Less CSS
-(use-package less-css-mode :mode "\\.less\\'")
+(use-package less-css-mode
+  :straight nil
+  :mode (".less" . less-css-mode))
 
 ;; Sass
 (use-package sass-mode
-  :commands (sass-mode)
-  :mode "\\.sass\\'"
-  :init
-  ;; (add-to-list 'auto-mode-alist '("\\.sass\\'" . sass-mode))
-  )
+  ;; :commands (sass-mode)
+  :defer t
+  :mode (".sass" . sass-mode)
+  :config
+  (require 'sass-mode))
 
 ;; Scss
 (use-package scss-mode
-  ;; :defer t
   :commands (scss-mode scss-compile css-mode web-mode)
-  :mode ("\\.scss" . scss-mode)
-  ;; :init
-  ;; (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+  :mode (".scss" . scss-mode)
   :config
   (require 'scss-mode)
-  (setq scss-sass-command
-        'dart-p
-        ;; (expand-file-name "~/.nix-profile/bin/sass")
-        ;; "sass"
-        )
-  (setq scss-compile-at-save nil) ;; Compile buffer at save
-  )
+
+  (setq scss-sass-command "sass")
+
+  ;; Compile buffer at save
+  (setq scss-compile-at-save nil))
 
 ;; Stylus
 (use-package stylus-mode :mode "\\.styl\\'")
@@ -62,18 +61,17 @@
 ;; CSS sort - https://github.com/jcs-elpa/com-css-sort
 (use-package com-css-sort
   :commands
-  (com-css-sort
-   com-css-sort-attributes-block
+  (com-css-sort-attributes-block
    com-css-sort-attributes-document)
   :config
   (progn
     ;; (setq com-css-sort-sort-type 'alphabetic-sort) ; default: 'type-sort
 
     ;; Sort attributes inside block
-    (define-key css-mode-map (kbd "C-k s") #'com-css-sort-attributes-block)
+    (define-key css-mode-map (kbd "C-c s s") #'com-css-sort-attributes-block)
 
     ;; Sort attributes through the whole document
-    (define-key css-mode-map (kbd "C-k d") #'com-css-sort-attributes-document)))
+    (define-key css-mode-map (kbd "C-c s d") #'com-css-sort-attributes-document)))
 
 ;; css-eldoc
 (use-package css-eldoc
@@ -81,4 +79,5 @@
   ;; add a hook if you want always to see the selector options in the minibuffer
   :config
   (add-hook 'css-mode-hook 'turn-on-css-eldoc)
+  (add-hook 'sass-mode-hook 'turn-on-css-eldoc)
   (add-hook 'scss-mode-hook 'turn-on-css-eldoc))
