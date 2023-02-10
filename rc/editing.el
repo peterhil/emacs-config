19ae90d3b5e30835e32575db7fb2c34eddc6a86b
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-10 22:23:27 +0200

    Disable linum-update-window advice as it gets stuck on Unicode characters

--- rc/editing.el
+++ rc/editing.el
@@ -38,11 +38,11 @@
 ;; Show line numbers, dynamically with spaces on either side:
 (global-linum-mode 1)
 
-(defadvice linum-update-window (around linum-dynamic activate)
-  (let* ((w (length (number-to-string
-                     (count-lines (point-min) (point-max)))))
-         (linum-format (concat " %" (number-to-string w) "d ")))
-    ad-do-it))
+;; (defadvice linum-update-window (around linum-dynamic activate)
+;;   (let* ((w (length (number-to-string
+;;                      (count-lines (point-min) (point-max)))))
+;;          (linum-format (concat " %" (number-to-string w) "d ")))
+;;     ad-do-it))
 
 ;; You may want to turn off linum for certain modes (this uses linum-off):
 (use-package linum-off)
