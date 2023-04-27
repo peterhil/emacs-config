754f1d172278060ef066b14d82d7337b042041c7
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-04-27 03:41:16 +0300

    Work around a bug in Emacs about linker warnings using '-undefined dynamic_lookup' options

--- rc/init/env.el
+++ rc/init/env.el
@@ -2,6 +2,13 @@
 ;; Environment setup
 ;; ------------------------------------------------------------------------------
 
+;; WORKAROUND a bug:
+;; "Warning (comp): ld: warning: -undefined dynamic_lookup may not work with chained fixups"
+;; https://www.reddit.com/r/emacs/comments/xfhnzz/weird_errors_with_latest_build_of_emacs/
+;; https://github.com/emacs-mirror/emacs/commit/97b928ce09d6034ebcb541fb548e5d4862302add
+(when (eq system-type 'darwin)
+  (customize-set-variable 'native-comp-driver-options '("-Wl,-w")))
+
 ;; TODO Use shell command `which emacs` to set this automatically.
 ;; See rc/reasonml.el for an example.
 (setq usr-prefix
