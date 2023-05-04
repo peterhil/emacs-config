f777b71d453d214289176aabbb3a927d22f67051
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-05-04 11:07:14 +0300

    Add my-reverse-text

--- rc/editing.el
+++ rc/editing.el
@@ -57,19 +57,28 @@
 
 
 ;; =============================================================================
-;; Automatically offer to create parent directories
-;; http://iqbalansari.me/blog/2014/12/07/automatically-create-parent-directories-on-visiting-a-new-file-in-emacs/
+;; Utils
 ;; -----------------------------------------------------------------------------
 
+(defun my-reverse-text (beg end)
+  "Reverse characters between BEG and END."
+  ;; https://emacs.stackexchange.com/a/38159/16904
+  (interactive "r")
+  (let ((region (buffer-substring beg end)))
+    (insert (nreverse
+             (delete-and-extract-region beg end)))))
+
+
 (defun my-create-non-existent-directory ()
+  "Automatically offer to create parent directories"
+  ;; http://iqbalansari.me/blog/2014/12/07/automatically-create-parent-directories-on-visiting-a-new-file-in-emacs/
   (let ((parent-directory (file-name-directory buffer-file-name)))
     (when (and (not (file-exists-p parent-directory))
                (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
       (make-directory parent-directory t))))
 
+
 (add-to-list 'find-file-not-found-functions #'my-create-non-existent-directory)
-
-
 
 
 ;; ==============================================================================
