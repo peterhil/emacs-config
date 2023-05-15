02cbb7e2700099a332ca078fa36a4817986b8c31
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-05-15 18:13:27 +0300

    Change undo key bindings

--- rc/undo-redo.el
+++ rc/undo-redo.el
@@ -13,10 +13,10 @@
   ;; Prevent undo tree files from polluting your project directories
   (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
 
-  (global-set-key (kbd "s-z g") 'global-undo-tree-mode)
-  (global-set-key (kbd "s-z t") 'undo-tree-visualize)
-  (define-key function-key-map (kbd "s-z z") 'undo)
-  (define-key function-key-map (kbd "s-z x") 'redo)
+  (global-set-key (kbd "M-s-z g") 'global-undo-tree-mode)
+  (global-set-key (kbd "M-s-z t") 'undo-tree-visualize)
+  (define-key function-key-map (kbd "M-s-z z") 'undo)
+  (define-key function-key-map (kbd "M-s-z x") 'redo)
   )
 
 
