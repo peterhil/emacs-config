55e40d6d02ddc0acc8a723642087ed5589ca24ee
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-18 11:30:48 +0200

    Fix and update paredit config

--- rc/paredit.el
+++ rc/paredit.el
@@ -2,16 +2,18 @@
 ;; Paredit
 ;; -----------------------------------------------------------------------------
 
+(defvar my-paredit-modes
+  '(emacs-lisp-mode
+    fennel-mode
+    lisp-mode
+    scheme-mode
+    slime-repl-mode))
+
 (use-package paredit
   :config
-  (show-paren-mode t)
   (use-package paredit-menu)
-  :hook ((emacs-lisp-mode-hook . 'paredit-mode)
-         (fennel-mode-hook . 'paredit-mode)
-         (lisp-mode-hook . 'paredit-mode)
-         (scheme-mode-hook . 'paredit-mode)
-         (slime-repl-mode-hook . 'paredit-mode)))
+  :hook
+  (my-paredit-modes . enable-paredit-mode))
 
-
-;; (use-package paredit-everywhere
-;;   :hook (prog-mode-hook . 'paredit-everywhere-mode))
+(use-package paredit-everywhere
+  :hook (prog-mode-hook . paredit-everywhere-mode))
