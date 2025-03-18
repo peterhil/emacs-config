6427d79adbc2768a2daa3c4f52e6f3ed7103236d
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-19 00:10:52 +0200

    Clean up Common Lisp and Slime config

--- rc/common-lisp.el
+++ rc/common-lisp.el
@@ -2,44 +2,48 @@
 ;;  Common Lisp
 ;; ------------------------------------------------------------------------------
 
-(eval-when-compile (require 'cl-lib))
+(eval-when-compile
+  (require 'cl-lib))
 
 ;; ==============================================================================
 ;; SLIME
 ;; ------------------------------------------------------------------------------
 
-(use-package slime)
+(setopt inferior-lisp-program (concat usr-prefix "/bin/sbcl"))
 
-(add-to-list 'load-path (concat my-system-site-lisp "slime"))
-;; (add-to-list 'load-path (concat user-emacs-directory "site-lisp/slime") ;; Already on path
-;; (add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/slime-v2.24/")
-(require 'slime-autoloads)
-
-(eval-after-load "slime"
-  '(progn
-     (slime-setup '(slime-asdf slime-tramp slime-fancy))
-     (slime-require 'swank-listener-hooks)))
-
-;; Set UTF-8 coding for swank/slime, see: https://answers.launchpad.net/sbcl/+question/74497
-(setopt slime-net-coding-system 'utf-8-unix)
-(setq language-environment "utf-8")
-
-;; Slime Lisp Implementations
-;; do m-- m-x slime ccl to start eg. Clozure Common Lisp from the list
+;; List of lternative implementations
+;; Press 'M-- M-x slime RET ccl' to start Clozure Common Lisp for example.
 (setq slime-lisp-implementations
       `((sbcl  (,(concat usr-prefix "/bin/sbcl")))
         (ccl   (,(concat usr-prefix "/bin/ccl")))
         (ccl64 (,(concat usr-prefix "/bin/ccl64")))
         (ecl   (,(concat usr-prefix "/bin/ecl")) :coding-system iso-latin-1-unix)))
 
-(setopt inferior-lisp-program (concat usr-prefix "/bin/sbcl"))
 
-(add-hook 'lisp-mode-hook
-          (lambda ()
-            (cond ((not (featurep 'slime))
-                   (require 'slime)
-                   (normal-mode)))))
+(setq site-lisp-slime-dir
+      (concat my-system-site-lisp "slime"))
 
-; Local CLHS
-;; (setq common-lisp-hyperspec-root
-;;       (concat usr-prefix "/share/doc/lisp/HyperSpec-7-0/HyperSpec/"))
+(defun require-slime ()
+  (when (featurep 'slime)
+    (require 'slime)
+    (normal-mode)))
+
+(use-package slime
+  :hook (lisp-mode . require-slime)
+  :load-path
+  site-lisp-slime-dir
+  ;; (concat user-emacs-directory "site-lisp/slime") ;; Already on path
+  ;; "~/quicklisp/dists/quicklisp/software/slime-v2.24/"
+  :config
+  ;; Set UTF-8 coding for swank/slime, see: https://answers.launchpad.net/sbcl/+question/74497
+  (setopt slime-net-coding-system 'utf-8-unix)
+  (setq language-environment "utf-8")
+
+  ;; Local CLHS
+  ;; (setq common-lisp-hyperspec-root
+  ;;       (concat usr-prefix "/share/doc/lisp/HyperSpec-7-0/HyperSpec/"))
+
+  (slime-setup '(slime-asdf slime-tramp slime-fancy))
+  (slime-require 'swank-listener-hooks)
+
+  (require 'slime-autoloads))
