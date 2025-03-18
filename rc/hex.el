269a680f841f270e27c8f699f63e3c1cdc15489b
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-18 23:31:56 +0200

    Remove hexview mode

--- rc/hex.el
+++ rc/hex.el
@@ -6,8 +6,3 @@
 ;; https://unix.stackexchange.com/q/18852
 (use-package nhexl-mode
   :bind ("C-c x" . nhexl-mode))
-
-
-;; Hexview mode does not have undo, but should be fast
-;; (require 'cl-lib)
-;; (use-package hexview-mode)
