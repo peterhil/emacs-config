f0ada6780434fdcd247199dbfbe75e770ff088e7
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-23 14:40:49 +0200

    Clean up avy.el

--- rc/avy.el
+++ rc/avy.el
@@ -6,14 +6,12 @@
 (use-package avy
   :config
   (avy-setup-default)
-
-  (global-set-key (kbd "C-.") 'avy-goto-char)
-  (global-set-key (kbd "C--") 'avy-goto-char-2)
+  :bind
+  ("C-." . avy-goto-char)
+  ("C--" . avy-goto-char-2)
+  ("C-c C-j" . avy-resume)
 
   ;; Jump line or word
-  (global-set-key (kbd "M-g f") 'avy-goto-line)
-  (global-set-key (kbd "M-g w") 'avy-goto-word-1)
-  (global-set-key (kbd "M-g e") 'avy-goto-word-0)
-
-  (global-set-key (kbd "C-c C-j") 'avy-resume)
-  )
+  ("M-g f" . avy-goto-line)
+  ("M-g e" . avy-goto-word-0)
+  ("M-g w" . avy-goto-word-1))
