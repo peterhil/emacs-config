3a7924105f06167383e9d65fafc8bbadd3ddd87d
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-23 16:07:40 +0200

    Use bind to initialize exec-path-from-shell

--- rc/shell.el
+++ rc/shell.el
@@ -5,7 +5,5 @@
 ;; Ensure that environment variables inside Emacs look the same
 ;; as in the user's shell: https://github.com/purcell/exec-path-from-shell
 (use-package exec-path-from-shell
-  :defer t
-  :if (memq window-system '(mac ns))
-  :init
-  (exec-path-from-shell-initialize))
+  :bind ("C-c s" . exec-path-from-shell-initialize)
+  :if (memq window-system '(mac ns)))
