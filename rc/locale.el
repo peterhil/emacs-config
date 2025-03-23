f9b5ca62b10409cbb531b1b908e36875cae4fd2d
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-23 02:45:29 +0200

    Clean up locale.el

--- rc/locale.el
+++ rc/locale.el
@@ -5,14 +5,14 @@
 ;; Language and text encoding
 
 (setopt locale-coding-system 'utf-8)
-(setopt read-quoted-char-radix 10)         ; use decimal, not octal
-
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8-unix)
-
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
+(setopt read-quoted-char-radix 10)       ; use decimal, not octal
 
 (set-language-environment "UTF-8")       ; prefer utf-8 for language settings
 (set-input-method nil)                   ; no funky input for normal editing
+
+(prefer-coding-system 'utf-8)
+
+(set-default-coding-systems 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
