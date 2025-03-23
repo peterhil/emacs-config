dd38c585f203ad9e43ea80ba13b7fbe7c618f7dd
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-23 02:34:28 +0200

    Replace after-init-hook with configure on init/main.el

--- rc/init/main.el
+++ rc/init/main.el
@@ -9,8 +9,4 @@
 
 ;; (configure "init/package")
 (configure "init/straight")
-
-(defun my-load-modules ()
-    (load (concat rc-dir "init/modules.el")))
-
-(add-hook 'after-init-hook 'my-load-modules)
+(configure "init/modules")
