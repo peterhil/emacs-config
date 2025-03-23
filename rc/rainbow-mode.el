4119e7da2d585ca5806bf4044422e27e0796c415
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-23 03:14:11 +0200

    Clean up rainbow-mode.el

--- rc/rainbow-mode.el
+++ rc/rainbow-mode.el
@@ -4,15 +4,11 @@
 ;;
 ;; https://julien.danjou.info/projects/emacs-packages#rainbow-mode
 
-;; (autoload 'rainbow-mode "rainbow-mode.el" "Minor mode for editing HTML colors" t)
-
 (use-package rainbow-mode
-  :mode (
-         "-theme\\.el\\'"
+  :mode ("-theme\\.el\\'"
          "\\.css\\'"
          "\\.html\\'"
          "\\.less\\'"
          "\\.sass\\'"
          "\\.scss\\'"
-         "\\.styl\\'"
-         ))
+         "\\.styl\\'"))
