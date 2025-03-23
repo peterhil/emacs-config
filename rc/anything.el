bd93ffaa100a1501c8abeb44e12218bc2baf378b
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-23 03:31:26 +0200

    Clean up anything.el

--- rc/anything.el
+++ rc/anything.el
@@ -4,10 +4,10 @@
 
 (use-package anything
   :bind ("C-x a n" . anything-mini)
-  :config
-  (setopt anything-command-map-prefix-key "C-x a")
-
+  :init
   ;; Fix errors about void variables
-  (setopt browse-url-galeon-program "firefox")
-  (setopt browse-url-mosaic-program "firefox")
-  (setopt browse-url-netscape-program "firefox"))
+  (setq browse-url-galeon-program "firefox"
+        browse-url-mosaic-program "firefox"
+            browse-url-netscape-program "firefox")
+  :config
+  (setopt anything-command-map-prefix-key "C-x a"))
