23858a7a180f8c485fd2ff30c6299bcb1b925421
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-12-31 01:36:26 +0200

    Use non-interactive shell with exec-path-from-shell

--- rc/shell.el
+++ rc/shell.el
@@ -12,4 +12,7 @@
           (daemonp))
   (use-package exec-path-from-shell
     :config
+    ;; Use non-interactive shell for faster startup
+    ;; https://github.com/purcell/exec-path-from-shell#making-exec-path-from-shell-faster
+    (setq exec-path-from-shell-arguments nil)
     (exec-path-from-shell-initialize)))
